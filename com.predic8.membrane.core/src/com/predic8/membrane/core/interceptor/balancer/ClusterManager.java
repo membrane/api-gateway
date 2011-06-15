e1c8f3447b4b251e5ef851bada1ff73f9aa14eb2
Shaan <info@predic8.de>
2011-06-15 14:12:22 +0000

    HashMap changed to Hashtable. sessionTimeout added. Delegation to Cluster added.

--- com.predic8.membrane.core/src/com/predic8/membrane/core/interceptor/balancer/ClusterManager.java
+++ com.predic8.membrane.core/src/com/predic8/membrane/core/interceptor/balancer/ClusterManager.java
@@ -7,8 +7,13 @@ import org.apache.commons.logging.*;
 public class ClusterManager {
 	private static Log log = LogFactory.getLog(ClusterManager.class.getName());
 	
-	Map<String, Cluster> clusters = new HashMap<String, Cluster>();
+	Map<String, Cluster> clusters = new Hashtable<String, Cluster>();
 	long timeout = 0;
+	long sessionTimeout = 2*60000;
+	
+	public ClusterManager() {
+		new SessionCleanupThread(clusters, sessionTimeout).start();
+	}
 	
 	public void up(String cName, String host, int port) {
 		getCluster(cName).nodeUp(new Node(host, port));
@@ -18,12 +23,20 @@ public class ClusterManager {
 		getCluster(cName).nodeDown(new Node(host, port));
 	}	
 	
-	public List<Node> getAllNodes(String cName) {
+	public List<Node> getAllNodesByCluster(String cName) {
 		return getCluster(cName).getAllNodes(timeout);
 	}
 
-	public List<Node> getAvailableNodes(String cName) {
+	public List<Node> getAvailableNodesByCluster(String cName) {
 		return getCluster(cName).getAvailableNodes(timeout);
+	}
+
+	public boolean containsSession(String cName, String sessionId) {
+		return getCluster(cName).containsSession(sessionId);
+	}
+	
+	public void addSession2Cluster(String sessionId, String cName, Node n) {
+		getCluster(cName).addSession(sessionId, n);
 	}
 	
 	private Cluster getCluster(String name) {
@@ -59,6 +72,22 @@ public class ClusterManager {
 
 	public Node getNode(String cluster, String host, int port) {
 		return getCluster(cluster).getNode(new Node(host,port));		
+	}
+
+	public Map<String, Session> getSessions(String cluster) {
+		return getCluster(cluster).getSessions();
+	}
+
+	public List<Session> getSessionsByNode(String cName, Node node) {
+		return getCluster(cName).getSessionsByNode(node);
+	}
+
+	public long getSessionTimeout() {
+		return sessionTimeout;
+	}
+
+	public void setSessionTimeout(long sessionTimeout) {
+		this.sessionTimeout = sessionTimeout;
 	}
 	
 	
