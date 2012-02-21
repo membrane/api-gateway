8f1aa51b26b3d0a0543d8e478f235c895c8db26d
Shaan <info@predic8.de>
2012-02-21 10:44:11 +0000

    Now creates temp dir if not existing.

--- membrane-esb/core/src/main/java/com/predic8/membrane/core/interceptor/statistics/StatisticsCSVInterceptor.java
+++ membrane-esb/core/src/main/java/com/predic8/membrane/core/interceptor/statistics/StatisticsCSVInterceptor.java
@@ -13,55 +13,52 @@
    limitations under the License. */
 package com.predic8.membrane.core.interceptor.statistics;
 
-import java.io.File;
-import java.io.FileWriter;
-import java.io.IOException;
+import java.io.*;
 
 import javax.xml.stream.*;
 
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
+import org.apache.commons.logging.*;
 
-import com.predic8.membrane.core.exchange.Exchange;
-import com.predic8.membrane.core.exchange.ExchangesUtil;
-import com.predic8.membrane.core.interceptor.AbstractInterceptor;
-import com.predic8.membrane.core.interceptor.Outcome;
+import com.predic8.membrane.core.exchange.*;
+import com.predic8.membrane.core.interceptor.*;
 
 public class StatisticsCSVInterceptor extends AbstractInterceptor {
 
-	private static Log log = LogFactory.getLog(StatisticsCSVInterceptor.class.getName());
-	
-	//private File csvFile;
+	private static Log log = LogFactory.getLog(StatisticsCSVInterceptor.class
+			.getName());
+
+	// private File csvFile;
 	private String fileName;
-	
+
 	public StatisticsCSVInterceptor() {
 		name = "CSV Logging";
 	}
-	
+
 	@Override
 	public Outcome handleResponse(Exchange exc) throws Exception {
-		log.debug("logging statistics to " + new File(fileName).getAbsolutePath());
+		log.debug("logging statistics to "
+				+ new File(fileName).getAbsolutePath());
 		writeExchange(exc);
 		return Outcome.CONTINUE;
 	}
 
 	private void writeExchange(Exchange exc) throws Exception {
-		FileWriter w = new FileWriter(fileName,true);
-		
-		writeCSV(ExchangesUtil.getStatusCode(exc),w);
-		writeCSV(ExchangesUtil.getTime(exc),w);
-		writeCSV(exc.getRule().toString(),w);
-		writeCSV(exc.getRequest().getMethod(),w);
-		writeCSV(exc.getRequest().getUri(),w);
-		writeCSV(exc.getSourceHostname(),w);
-		writeCSV(exc.getServer(),w);
-		writeCSV(exc.getRequestContentType(),w);
-		writeCSV(ExchangesUtil.getRequestContentLength(exc),w);
-		writeCSV(ExchangesUtil.getResponseContentType(exc),w);
-		writeCSV(ExchangesUtil.getResponseContentLength(exc),w);
-		writeCSV(ExchangesUtil.getTimeDifference(exc),w);
+		FileWriter w = new FileWriter(fileName, true);
+
+		writeCSV(ExchangesUtil.getStatusCode(exc), w);
+		writeCSV(ExchangesUtil.getTime(exc), w);
+		writeCSV(exc.getRule().toString(), w);
+		writeCSV(exc.getRequest().getMethod(), w);
+		writeCSV(exc.getRequest().getUri(), w);
+		writeCSV(exc.getSourceHostname(), w);
+		writeCSV(exc.getServer(), w);
+		writeCSV(exc.getRequestContentType(), w);
+		writeCSV(ExchangesUtil.getRequestContentLength(exc), w);
+		writeCSV(ExchangesUtil.getResponseContentType(exc), w);
+		writeCSV(ExchangesUtil.getResponseContentLength(exc), w);
+		writeCSV(ExchangesUtil.getTimeDifference(exc), w);
 		writeNewLine(w);
-		
+
 		w.close();
 	}
 
@@ -72,13 +69,16 @@ public class StatisticsCSVInterceptor extends AbstractInterceptor {
 
 	private void createCSVFile() throws Exception {
 		File csvFile = new File(fileName);
-		log.debug("creating csv file at "+csvFile.getAbsolutePath());
-		
+		log.debug("creating csv file at " + csvFile.getAbsolutePath());
+
+		if (csvFile.getParentFile() != null) {
+			csvFile.getParentFile().mkdirs();
+		}
 		csvFile.createNewFile();
-		
+
 		if (!csvFile.canWrite())
 			throw new IOException("File " + fileName + " is not writable.");
-		
+
 		if (csvFile.length() == 0)
 			writeHeaders();
 	}
@@ -86,47 +86,47 @@ public class StatisticsCSVInterceptor extends AbstractInterceptor {
 	public String getFileName() {
 		return new File(fileName).getName();
 	}
-	
+
 	private void writeCSV(String value, FileWriter w) throws IOException {
-		w.append(value+";");
+		w.append(value + ";");
 	}
-	
+
 	private void writeNewLine(FileWriter w) throws IOException {
 		w.append(System.getProperty("line.separator"));
 	}
-	
+
 	private void writeHeaders() throws Exception {
-		FileWriter w = new FileWriter(fileName,true);
-		
-		writeCSV("Status Code",w);
-		writeCSV("Time",w);
-		writeCSV("Rule",w);
-		writeCSV("Method",w);
-		writeCSV("Path",w);
-		writeCSV("Client",w);
-		writeCSV("Server",w);
-		writeCSV("Request Content-Type",w);
-		writeCSV("Request Content Length",w);
-		writeCSV("Response Content-Type",w);
-		writeCSV("Response Content Length",w);
-		writeCSV("Duration",w);
+		FileWriter w = new FileWriter(fileName, true);
+
+		writeCSV("Status Code", w);
+		writeCSV("Time", w);
+		writeCSV("Rule", w);
+		writeCSV("Method", w);
+		writeCSV("Path", w);
+		writeCSV("Client", w);
+		writeCSV("Server", w);
+		writeCSV("Request Content-Type", w);
+		writeCSV("Request Content Length", w);
+		writeCSV("Response Content-Type", w);
+		writeCSV("Response Content Length", w);
+		writeCSV("Duration", w);
 		writeNewLine(w);
-		
+
 		w.close();
 	}
 
 	@Override
 	protected void writeInterceptor(XMLStreamWriter out)
 			throws XMLStreamException {
-		
+
 		out.writeStartElement("statisticsCSV");
 		out.writeAttribute("file", fileName);
-		
+
 		out.writeEndElement();
 	}
-	
+
 	@Override
-	protected void parseAttributes(XMLStreamReader token) throws Exception {		
+	protected void parseAttributes(XMLStreamReader token) throws Exception {
 		setFileName(token.getAttributeValue("", "file"));
 	}
 }
