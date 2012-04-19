70c83626ca4bab51af839be283b712f2f9cb6d8b
Tobias Polley <polley@predic8.de>
2012-04-19 13:53:27 +0000

    fixed race condition when writing CSV file

--- membrane-esb/core/src/main/java/com/predic8/membrane/core/interceptor/statistics/StatisticsCSVInterceptor.java
+++ membrane-esb/core/src/main/java/com/predic8/membrane/core/interceptor/statistics/StatisticsCSVInterceptor.java
@@ -13,15 +13,25 @@
    limitations under the License. */
 package com.predic8.membrane.core.interceptor.statistics;
 
-import java.io.*;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.util.HashMap;
+import java.util.Map;
 
-import javax.xml.stream.*;
+import javax.xml.stream.XMLStreamException;
+import javax.xml.stream.XMLStreamReader;
+import javax.xml.stream.XMLStreamWriter;
 
-import org.apache.commons.logging.*;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 
 import com.predic8.membrane.core.Constants;
-import com.predic8.membrane.core.exchange.*;
-import com.predic8.membrane.core.interceptor.*;
+import com.predic8.membrane.core.exchange.Exchange;
+import com.predic8.membrane.core.exchange.ExchangesUtil;
+import com.predic8.membrane.core.interceptor.AbstractInterceptor;
+import com.predic8.membrane.core.interceptor.Outcome;
 
 /**
  * The output file is UTF-8 encoded.
@@ -31,7 +41,12 @@ public class StatisticsCSVInterceptor extends AbstractInterceptor {
 	private static Log log = LogFactory.getLog(StatisticsCSVInterceptor.class
 			.getName());
 
-	// private File csvFile;
+	// maps all fileName objects used by instances of this class to themselves.
+	// used to get unique String instances of the same file name 
+	// (when two instances use the same file name)
+	private static final Map<String, String> fileNames = new HashMap<String, String>();
+	
+	// the file name of the log file; at the same time a lock guarding the lock file
 	private String fileName;
 
 	public StatisticsCSVInterceptor() {
@@ -47,48 +62,59 @@ public class StatisticsCSVInterceptor extends AbstractInterceptor {
 	}
 
 	private void writeExchange(Exchange exc) throws Exception {
-		FileOutputStream fos = new FileOutputStream(fileName, true);
-		try {
-			OutputStreamWriter w = new OutputStreamWriter(fos, Constants.UTF_8_CHARSET);
+		synchronized(fileName) {
+			FileOutputStream fos = new FileOutputStream(fileName, true);
+			try {
+				OutputStreamWriter w = new OutputStreamWriter(fos, Constants.UTF_8_CHARSET);
 
-			writeCSV(ExchangesUtil.getStatusCode(exc), w);
-			writeCSV(ExchangesUtil.getTime(exc), w);
-			writeCSV(exc.getRule().toString(), w);
-			writeCSV(exc.getRequest().getMethod(), w);
-			writeCSV(exc.getRequest().getUri(), w);
-			writeCSV(exc.getSourceHostname(), w);
-			writeCSV(exc.getServer(), w);
-			writeCSV(exc.getRequestContentType(), w);
-			writeCSV(ExchangesUtil.getRequestContentLength(exc), w);
-			writeCSV(ExchangesUtil.getResponseContentType(exc), w);
-			writeCSV(ExchangesUtil.getResponseContentLength(exc), w);
-			writeCSV(ExchangesUtil.getTimeDifference(exc), w);
-			writeNewLine(w);
-			w.flush();
-		} finally {
-			fos.close();
+				writeCSV(ExchangesUtil.getStatusCode(exc), w);
+				writeCSV(ExchangesUtil.getTime(exc), w);
+				writeCSV(exc.getRule().toString(), w);
+				writeCSV(exc.getRequest().getMethod(), w);
+				writeCSV(exc.getRequest().getUri(), w);
+				writeCSV(exc.getSourceHostname(), w);
+				writeCSV(exc.getServer(), w);
+				writeCSV(exc.getRequestContentType(), w);
+				writeCSV(ExchangesUtil.getRequestContentLength(exc), w);
+				writeCSV(ExchangesUtil.getResponseContentType(exc), w);
+				writeCSV(ExchangesUtil.getResponseContentLength(exc), w);
+				writeCSV(ExchangesUtil.getTimeDifference(exc), w);
+				writeNewLine(w);
+				w.flush();
+			} finally {
+				fos.close();
+			}
 		}
 	}
 
 	public void setFileName(String fileName) throws Exception {
-		this.fileName = fileName;
-		createCSVFile();
+		synchronized(fileNames) {
+			String fn = fileNames.get(fileName);
+			if (fn != null)
+				fileName = fn;
+			else
+				fileNames.put(fileName, fileName);
+			this.fileName = fileName;
+			createCSVFile();
+		}
 	}
 
 	private void createCSVFile() throws Exception {
-		File csvFile = new File(fileName);
-		log.debug("creating csv file at " + csvFile.getAbsolutePath());
+		synchronized (fileName) {
+			File csvFile = new File(fileName);
+			log.debug("creating csv file at " + csvFile.getAbsolutePath());
 
-		if (csvFile.getParentFile() != null) {
-			csvFile.getParentFile().mkdirs();
+			if (csvFile.getParentFile() != null) {
+				csvFile.getParentFile().mkdirs();
+			}
+			csvFile.createNewFile();
+
+			if (!csvFile.canWrite())
+				throw new IOException("File " + fileName + " is not writable.");
+
+			if (csvFile.length() == 0)
+				writeHeaders();
 		}
-		csvFile.createNewFile();
-
-		if (!csvFile.canWrite())
-			throw new IOException("File " + fileName + " is not writable.");
-
-		if (csvFile.length() == 0)
-			writeHeaders();
 	}
 
 	public String getFileName() {
@@ -104,26 +130,28 @@ public class StatisticsCSVInterceptor extends AbstractInterceptor {
 	}
 
 	private void writeHeaders() throws Exception {
-		FileOutputStream fos = new FileOutputStream(fileName, true);
-		try {
-			OutputStreamWriter w = new OutputStreamWriter(fos, Constants.UTF_8_CHARSET);
+		synchronized(fileName) {
+			FileOutputStream fos = new FileOutputStream(fileName, true);
+			try {
+				OutputStreamWriter w = new OutputStreamWriter(fos, Constants.UTF_8_CHARSET);
 
-			writeCSV("Status Code", w);
-			writeCSV("Time", w);
-			writeCSV("Rule", w);
-			writeCSV("Method", w);
-			writeCSV("Path", w);
-			writeCSV("Client", w);
-			writeCSV("Server", w);
-			writeCSV("Request Content-Type", w);
-			writeCSV("Request Content Length", w);
-			writeCSV("Response Content-Type", w);
-			writeCSV("Response Content Length", w);
-			writeCSV("Duration", w);
-			writeNewLine(w);
-			w.flush();
-		} finally {
-			fos.close();
+				writeCSV("Status Code", w);
+				writeCSV("Time", w);
+				writeCSV("Rule", w);
+				writeCSV("Method", w);
+				writeCSV("Path", w);
+				writeCSV("Client", w);
+				writeCSV("Server", w);
+				writeCSV("Request Content-Type", w);
+				writeCSV("Request Content Length", w);
+				writeCSV("Response Content-Type", w);
+				writeCSV("Response Content Length", w);
+				writeCSV("Duration", w);
+				writeNewLine(w);
+				w.flush();
+			} finally {
+				fos.close();
+			}
 		}
 	}
 
