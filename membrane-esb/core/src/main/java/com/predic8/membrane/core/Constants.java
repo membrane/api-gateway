47b18fabe70a9236cb19af60d16bcd5490b2cc10
Shaan <info@predic8.de>
2012-06-26 14:44:15 +0000

    MessageAnalyser created

--- membrane-esb/core/src/main/java/com/predic8/membrane/core/Constants.java
+++ membrane-esb/core/src/main/java/com/predic8/membrane/core/Constants.java
@@ -64,6 +64,8 @@ public class Constants {
 
 	public static final String SOAP11_NS = "http://schemas.xmlsoap.org/soap/envelope/";
 	public static final String SOAP12_NS = "http://www.w3.org/2003/05/soap-envelope";
+	public static final String SOAP11_VERION = "1.1";
+	public static final String SOAP12_VERION = "1.2";
 
 	public static final String PROTOCOL_SOAP11 = "SOAP11";
 
