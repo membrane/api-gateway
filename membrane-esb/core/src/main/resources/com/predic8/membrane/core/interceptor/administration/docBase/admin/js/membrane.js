3e8d572ec587ac14b16820034ee3b5af2f91b5df
Shaan <info@predic8.de>
2012-06-26 15:01:21 +0000

    added message statistics to admin console

--- membrane-esb/core/src/main/resources/com/predic8/membrane/core/interceptor/administration/docBase/admin/js/membrane.js
+++ membrane-esb/core/src/main/resources/com/predic8/membrane/core/interceptor/administration/docBase/admin/js/membrane.js
@@ -4,6 +4,54 @@ $(function() {
 	  'sPaginationType': 'full_numbers'
 	});
 	
+	$('#message-stat-table').dataTable({
+		  'bJQueryUI': true,
+		  "bProcessing": true,
+		  "bServerSide": true,
+		  "bDestroy":true,
+		  "sAjaxSource": '../statistics',
+		  "sAjaxDataProp": "statistics",
+		  "aoColumns": [
+		                { "mDataProp": "statusCode" },
+		                { "mDataProp": "time" },
+		                { "mDataProp": "rule" },
+		                { "mDataProp": "method" },
+		                { "mDataProp": "path" },
+		                { "mDataProp": "client" },
+		                { "mDataProp": "server" },
+		                { "mDataProp": "reqContentType" },
+		                { "mDataProp": "reqContentLenght" },
+		                { "mDataProp": "respContentType" },
+		                { "mDataProp": "respContentLenght" },
+		                { "mDataProp": "duration" }
+		              ],
+          "fnServerData": function ( sSource, aoData, fnCallback ) {
+        	  console.log(aoData);
+        	  function getParam(name) {
+        		  function byName(it) {
+        			  return it.name == name;
+        		  }
+        		  return $.grep(aoData, byName)[0].value;
+        	  }
+        	  
+              $.ajax( {            	  
+                "dataType": 'json', 
+                "type": "GET", 
+                "url": sSource, 
+                "data": [{name:'offset', value:getParam('iDisplayStart')}, 
+                         {name:'max', value:getParam('iDisplayLength')},
+                         {name:'sort', value:getParam('mDataProp_'+getParam('iSortCol_0'))},
+                         {name:'order', value:getParam('sSortDir_0')}], 
+                "success": function(data) {   
+                	data.sEcho = aoData.sEcho;
+                	data.iTotalRecords = data.total;
+                	data.iTotalDisplayRecords = data.total;
+                	fnCallback(data);
+                }
+              } );
+            }		              
+	});
+
 	/*	
 	$('table.sortPrio').dataTable({
 	  'bJQueryUI': true,
