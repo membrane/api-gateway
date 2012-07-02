ae2cf43b97f4003777e607dd4b8a3a364d359c1c
Shaan <info@predic8.de>
2012-07-02 14:19:33 +0000

    -extended rest api to obtain client statistics -added default parameters -removed links from json responses -renamed some json properties -added more filter options

--- membrane-esb/core/src/main/java/com/predic8/membrane/core/interceptor/administration/AdminRESTInterceptor.java
+++ membrane-esb/core/src/main/java/com/predic8/membrane/core/interceptor/administration/AdminRESTInterceptor.java
@@ -1,7 +1,5 @@
 package com.predic8.membrane.core.interceptor.administration;
 
-import static com.predic8.membrane.core.util.URLParamUtil.createQueryString;
-
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.URLEncoder;
@@ -16,6 +14,7 @@ import org.codehaus.jackson.JsonGenerator;
 
 import com.predic8.membrane.core.exchange.AbstractExchange;
 import com.predic8.membrane.core.exchange.ExchangesUtil;
+import com.predic8.membrane.core.exchangestore.ClientStatistics;
 import com.predic8.membrane.core.http.HeaderField;
 import com.predic8.membrane.core.http.Message;
 import com.predic8.membrane.core.http.MimeType;
@@ -31,14 +30,48 @@ import com.predic8.membrane.core.util.TextUtil;
 
 public class AdminRESTInterceptor extends RESTInterceptor {
 
-	@Mapping("/admin/rest/proxies/?(\\?.*)?")
+	@Mapping("/admin/rest/clients(/?\\?.*)?")
+	public Response getClients(QueryParameter params, String relativeRootPath) throws Exception {
+		final List<? extends ClientStatistics> clients = getRouter().getExchangeStore().getClientStatistics();
+
+		Collections.sort(
+				clients,
+				ComparatorFactory.getClientStatisticsComparator(params.getString("sort", "name"),
+						params.getString("order", "asc")));
+
+		int offset = params.getInt("offset", 0);
+		int max = params.getInt("max", clients.size());
+
+		final List<? extends ClientStatistics> paginated = clients.subList(offset,
+				Math.min(offset + max, clients.size()));
+
+		return json( new JSONContent() {
+			public void write(JsonGenerator gen) throws Exception {
+				gen.writeStartObject();
+					gen.writeArrayFieldStart("clients");
+						for (ClientStatistics s : paginated) {
+							gen.writeStartObject();
+								gen.writeStringField("name", s.getClient());
+								gen.writeNumberField("count", s.getCount());
+								gen.writeNumberField("min", s.getMinDuration());
+								gen.writeNumberField("max", s.getMaxDuration());
+								gen.writeNumberField("avg", s.getAvgDuration());
+							gen.writeEndObject();
+						}					
+					gen.writeEndArray();
+				gen.writeEndObject();
+			}
+		});
+	}
+	
+	@Mapping("/admin/rest/proxies(/?\\?.*)?")
 	public Response getProxies(QueryParameter params, String relativeRootPath) throws Exception {
 		final List<ServiceProxy> proxies = getServiceProxies();
 
 		Collections.sort(
 				proxies,
-				ComparatorFactory.getServiceProxyComparator(params.getString("sort"),
-						params.getString("order")));
+				ComparatorFactory.getServiceProxyComparator(params.getString("sort", "name"),
+						params.getString("order", "asc")));
 
 		int offset = params.getInt("offset", 0);
 		int max = params.getInt("max", proxies.size());
@@ -60,7 +93,6 @@ public class AdminRESTInterceptor extends RESTInterceptor {
 						gen.writeStringField("targetHost", p.getTargetHost());
 						gen.writeNumberField("targetPort", p.getTargetPort());
 						gen.writeNumberField("count", p.getCount());
-						gen.writeStringField("details", AdminPageBuilder.createHRef("service-proxy", "show", createQueryString("name",RuleUtil.getRuleIdentifier(p))));
 						gen.writeObjectFieldStart("actions");
 							if (!isReadOnly()) {
 								gen.writeStringField("delete", "/admin/service-proxy/delete?name="+URLEncoder.encode(RuleUtil.getRuleIdentifier(p),"UTF-8"));
@@ -75,15 +107,6 @@ public class AdminRESTInterceptor extends RESTInterceptor {
 		});
 	}
 	
-	private List<ServiceProxy> getServiceProxies() {
-		List<ServiceProxy> rules = new LinkedList<ServiceProxy>();
-		for (Rule r : router.getRuleManager().getRules()) {
-			if (!(r instanceof ServiceProxy)) continue;
-			rules.add((ServiceProxy) r);
-		}			
-		return rules;
-	}
-
 	@Mapping("/admin/rest/exchanges/(\\d+)/(response|request)/raw")
 	public Response getRaw(QueryParameter params, String relativeRootPath) throws Exception {
 		AbstractExchange exc = router.getExchangeStore().getExchangeById(params.getGroupInt(1));
@@ -119,14 +142,6 @@ public class AdminRESTInterceptor extends RESTInterceptor {
 	@Mapping("/admin/rest/exchanges/(\\d+)/(response|request)/body")
 	public Response getRequestBody(QueryParameter params, String relativeRootPath) throws Exception {
 		AbstractExchange exc = router.getExchangeStore().getExchangeById(params.getGroupInt(1));
-
-//		System.out.println("********************");
-//		synchronized (router.getExchangeStore()) {
-//			for (AbstractExchange ae : router.getExchangeStore().getAllExchangesAsList()) {
-//				System.out.println(ae.hashCode());
-//			}
-//		}
-//		System.out.println("********************");
 
 		if (exc== null) {
 			return Response.notFound().build();
@@ -182,23 +197,23 @@ public class AdminRESTInterceptor extends RESTInterceptor {
 
 		return json( new JSONContent() {
 			public void write(JsonGenerator gen) throws Exception {
-				writeRecord(exc, gen);
+				writeExchange(exc, gen);
 			}
 		});
 	}
 
-	@Mapping("/admin/rest/exchanges/?(\\?.*)?")
+	@Mapping("/admin/rest/exchanges(/?\\?.*)?")
 	public Response getExchanges(QueryParameter params, String relativeRootPath) throws Exception {
 		
 		List<AbstractExchange> exchanges = new ArrayList<AbstractExchange>(
 				getRouter().getExchangeStore().getAllExchangesAsList());
 		
-		exchanges = filter(params.getString("proxy"), exchanges);
+		exchanges = filter(params, exchanges);
 		
 		Collections.sort(
 				exchanges,
-				ComparatorFactory.getAbstractExchangeComparator(params.getString("sort"),
-						params.getString("order")));
+				ComparatorFactory.getAbstractExchangeComparator(params.getString("sort", "time"),
+						params.getString("order", "desc")));
 
 		int offset = params.getInt("offset", 0);
 		int max = params.getInt("max", exchanges.size());
@@ -212,7 +227,7 @@ public class AdminRESTInterceptor extends RESTInterceptor {
 				gen.writeStartObject();
 				gen.writeArrayFieldStart("exchanges");
 					for (AbstractExchange e : paginated) {
-						writeRecord(e, gen);
+						writeExchange(e, gen);
 					}					
 					gen.writeEndArray();
 					gen.writeNumberField("total", total);
@@ -221,47 +236,70 @@ public class AdminRESTInterceptor extends RESTInterceptor {
 		});
 	}
 
-	private List<AbstractExchange> filter(String proxy,
+	private List<AbstractExchange> filter(QueryParameter params,
 			List<AbstractExchange> exchanges) throws Exception {
 
-		if (proxy == null) {
-			return exchanges;
-		}
-
 		List<AbstractExchange> list = new ArrayList<AbstractExchange>();
-		synchronized (exchanges) {
-			for (AbstractExchange e : exchanges) {
-				if (e.getRule().toString().equals(proxy)) {
-					list.add(e);
-				}
+		for (AbstractExchange e : exchanges) {
+			if ((!params.has("proxy") || e.getRule().toString().equals(params.getString("proxy"))) &&
+			    (!params.has("statuscode") || e.getResponse().getStatusCode() == params.getInt("statuscode")) &&
+				(!params.has("client") || e.getSourceHostname().equals(params.getString("client"))) &&
+				(!params.has("server") || params.getString("server").equals(e.getServer()==null?"":e.getServer())) &&
+				(!params.has("method") || e.getRequest().getMethod().equals(params.getString("method"))) &&
+				(!params.has("reqcontenttype") || e.getRequestContentType().equals(params.getString("reqcontenttype"))) &&
+				(!params.has("respcontenttype") || e.getResponseContentType().equals(params.getString("respcontenttype")))) {
+				list.add(e);
 			}
 		}
 		return list;
 	}
 
-	private void writeRecord(AbstractExchange exc, JsonGenerator jsonGen)
+	private void writeExchange(AbstractExchange exc, JsonGenerator gen)
 			throws IOException, JsonGenerationException, SQLException {
-		jsonGen.writeStartObject();
-		jsonGen.writeNumberField("id", exc.hashCode());
-		jsonGen.writeNumberField("statusCode", exc.getResponse()
+		gen.writeStartObject();
+		gen.writeNumberField("id", exc.hashCode());
+		gen.writeNumberField("statusCode", exc.getResponse()
 				.getStatusCode());
-		jsonGen.writeStringField("time", ExchangesUtil.getTime(exc));
-		jsonGen.writeStringField("rule", exc.getRule().toString());
-		jsonGen.writeStringField("method", exc.getRequest().getMethod());
-		jsonGen.writeStringField("path", exc.getRequest().getUri());
-		jsonGen.writeStringField("client", exc.getSourceHostname());
-		jsonGen.writeStringField("server", exc.getServer());
-		jsonGen.writeStringField("reqContentType", exc.getRequestContentType());
-		jsonGen.writeNumberField("reqContentLenght",
-				exc.getRequestContentLength());
-		jsonGen.writeStringField("respContentType",
+		gen.writeStringField("time", ExchangesUtil.getTime(exc));
+		gen.writeStringField("proxy", exc.getRule().toString());
+		gen.writeNumberField("listenPort", exc.getRule().getKey().getPort());
+		gen.writeStringField("method", exc.getRequest().getMethod());
+		gen.writeStringField("path", exc.getRequest().getUri());
+		gen.writeStringField("client", exc.getSourceHostname());
+		gen.writeStringField("server", exc.getServer());
+		gen.writeNumberField("serverPort",  getServerPort(exc));		
+		gen.writeStringField("reqContentType", exc.getRequestContentType());
+		if (exc.getRequestContentLength()!=-1) {
+			gen.writeNumberField("reqContentLength", exc.getRequestContentLength());
+		} else {
+			gen.writeNullField("reqContentLength");
+		}
+
+		gen.writeStringField("respContentType", exc.getResponseContentType());
+		gen.writeStringField("respContentLength",
 				exc.getResponseContentType());
-		jsonGen.writeNumberField("respContentLenght",
-				exc.getResponseContentLength());
-		jsonGen.writeNumberField("duration",
+		if (exc.getResponseContentLength()!=-1) {
+			gen.writeNumberField("respContentLength", exc.getResponseContentLength());
+		} else {
+			gen.writeNullField("respContentLength");
+		}
+
+		gen.writeNumberField("duration",
 				exc.getTimeResReceived() - exc.getTimeReqSent());
-		jsonGen.writeStringField("msgFilePath", JDBCUtil.getFilePath(exc));
-		jsonGen.writeEndObject();
+		gen.writeStringField("msgFilePath", JDBCUtil.getFilePath(exc));
+		gen.writeEndObject();
 	}
 
+	private int getServerPort(AbstractExchange exc) {
+		return exc.getRule()instanceof ServiceProxy?((ServiceProxy) exc.getRule()).getTargetPort():-1;
+	}
+
+	private List<ServiceProxy> getServiceProxies() {
+		List<ServiceProxy> rules = new LinkedList<ServiceProxy>();
+		for (Rule r : router.getRuleManager().getRules()) {
+			if (!(r instanceof ServiceProxy)) continue;
+			rules.add((ServiceProxy) r);
+		}			
+		return rules;
+	}
 }
