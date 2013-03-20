d9b258eb3a5e2d5bb0804955edf3d90d6d98eccc
Alexander Melnyk <a.schroet@gmail.com>
2013-03-20 16:59:37 +0100

    fixed building issues

--- osgi/src/main/java/com/predic8/membrane/osgi/CoreActivator.java
+++ osgi/src/main/java/com/predic8/membrane/osgi/CoreActivator.java
@@ -14,13 +14,18 @@
 
 package com.predic8.membrane.osgi;
 
-import java.io.*;
-import java.net.*;
-import java.util.concurrent.Executors;
+import java.io.File;
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URLClassLoader;
 
 import org.apache.commons.lang.ArrayUtils;
-import org.apache.commons.logging.*;
-import org.eclipse.core.runtime.*;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.eclipse.core.runtime.FileLocator;
+import org.eclipse.core.runtime.ILogListener;
+import org.eclipse.core.runtime.Platform;
+import org.eclipse.core.runtime.Plugin;
 import org.osgi.framework.BundleContext;
 
 import com.predic8.membrane.core.ClassloaderUtil;
@@ -39,118 +44,116 @@ public class CoreActivator extends Plugin {
 
 	private static Log log = LogFactory.getLog(CoreActivator.class.getName());
 
-	// The shared instance
-	private static CoreActivator plugin; // TODO: make non-static
 	private Router router;
 
 	private ILogListener logListener;
-	
+
 	public CoreActivator() {
 		logListener = new MembraneLogListener();
 	}
 
 	public void start(BundleContext context) throws Exception {
 		super.start(context);
-		plugin = this;
 
 		Platform.addLogListener(logListener);
-		
+
 		final MembraneCommandLine cl = new MembraneCommandLine();
 		cl.parse(fixArguments(Platform.getCommandLineArgs()));
-		
-		if (cl.hasMonitorBeans()) {
-			log.info("loading monitor beans from command line argument: "+cl.getMonitorBeans());
-			router = Router.init(new File(cl.getMonitorBeans()).getAbsolutePath(), 
-						this.getClass().getClassLoader());
-		} else {		
+
+		if (cl.hasConfiguration()) {
+			log.info("loading monitor beans from command line argument: "
+					+ getConfigurationFileName(cl));
+			router = Router.init(getConfigurationFileName(cl), this.getClass()
+					.getClassLoader());
+		} else {
 			try {
-				if (ClassloaderUtil.fileExists(getMonitorBeansFileName())) {
-					log.info("Eclipse framework found config file: " + getMonitorBeansFileName());
+				if (ClassloaderUtil.fileExists(getConfigurationFileName())) {
+					log.info("Eclipse framework found config file: "
+							+ getConfigurationFileName());
 					readBeanConfigWhenStartedAsProduct();
 				} else {
 					readBeanConfigWhenStartedInEclipse();
 				}
 			} catch (Exception e1) {
-				log.error("Unable to read bean configuration file: " + e1.getMessage());
-				log.error("Unable to read bean configuration file: " + e1.getStackTrace());
+				log.error("Unable to read bean configuration file: "
+						+ e1.getMessage());
+				log.error("Unable to read bean configuration file: "
+						+ e1.getStackTrace());
 				e1.printStackTrace();
 			}
 		}
-
-		Executors.newSingleThreadExecutor().execute(new Runnable() {
-			public void run() {
-				try {
-					router.getConfigurationManager().loadConfiguration(getConfigurationFileName(cl));
-				} catch (Exception e) {
-					log.warn("no configuration loaded", e);
-					// we ignore this exception because the monitor can start up
-					// without loading a rules configuration
-					// we need to throw an exception because the router must display an error message
-				}
-			}
-
-			private String getConfigurationFileName(MembraneCommandLine cl) {
-				if (cl.hasConfiguration()) {
-					log.info("loading configuration from command line argument: "+cl.getConfiguration());
-					return new File(cl.getConfiguration()).getAbsolutePath();
-				}
-				return System.getProperty("user.home") + System.getProperty("file.separator") + ".membrane.xml";
-			}
-		});
 	}
 
 	/*
-	 * Removes the string "-product" and the next string so that MembraneCommandLine don't get a parsing error. 
+	 * Removes the string "-product" and the next string so that
+	 * MembraneCommandLine don't get a parsing error.
 	 */
 	private String[] fixArguments(String[] args) {
 		int i = ArrayUtils.indexOf(args, "-product");
 		if (i == -1)
 			return args;
-		
-		return (String[])ArrayUtils.remove( (String[])ArrayUtils.remove(args, i),i);
+
+		return (String[]) ArrayUtils.remove(
+				(String[]) ArrayUtils.remove(args, i), i);
+	}
+
+	private String getConfigurationFileName(MembraneCommandLine cl) {
+		if (cl.hasConfiguration()) {
+			log.info("loading configuration from command line argument: "
+					+ cl.getConfiguration());
+			return new File(cl.getConfiguration()).getAbsolutePath();
+		}
+		return System.getProperty("user.home")
+				+ System.getProperty("file.separator") + ".membrane.xml";
 	}
 
 	private void readBeanConfigWhenStartedAsProduct() throws Exception {
-		log.info("Reading router configuration from " + getMonitorBeansFileName());
+		log.info("Reading router configuration from "
+				+ getConfigurationFileName());
 		log.info("Project root: " + getProjectRoot());
-		
-		URLClassLoader externalClassloader = ClassloaderUtil.getExternalClassloader(getProjectRoot());
-		//DataSource was unable to load oracle JDBC driver class with external class loader
-		//issue at Spring forum:  http://forum.springframework.org/showthread.php?t=11141
+
+		URLClassLoader externalClassloader = ClassloaderUtil
+				.getExternalClassloader(getProjectRoot());
+		// DataSource was unable to load oracle JDBC driver class with external
+		// class loader
+		// issue at Spring forum:
+		// http://forum.springframework.org/showthread.php?t=11141
 		Thread.currentThread().setContextClassLoader(externalClassloader);
-		
-		router = Router.init(getMonitorBeansFileName(), externalClassloader );
+
+		router = Router.init(getConfigurationFileName(), externalClassloader);
 		log.info("Router instance: " + router);
 	}
 
-	private void readBeanConfigWhenStartedInEclipse() throws MalformedURLException {
-		log.info("Reading configuration from configuration/monitor-beans.xml");
+	private void readBeanConfigWhenStartedInEclipse()
+			throws MalformedURLException {
+		log.info("Reading configuration from configuration/proxies.xml");
 
 		String membraneHome = System.getenv(Constants.MEMBRANE_HOME);
 		if (membraneHome == null)
-			throw new IllegalStateException("MEMBRANE_HOME not set"); 		
-		
-		router = Router.init("file:" + membraneHome + System.getProperty("file.separator") + "configuration" + System.getProperty("file.separator") + "monitor-beans.xml", this.getClass().getClassLoader());
+			throw new IllegalStateException("MEMBRANE_HOME not set");
+
+		router = Router.init(
+				"file:" + membraneHome + System.getProperty("file.separator")
+						+ "configuration"
+						+ System.getProperty("file.separator") + "proxies.xml",
+				this.getClass().getClassLoader());
 	}
 
-	private String getMonitorBeansFileName() throws IOException {
-		return getProjectRoot() + System.getProperty("file.separator") + "configuration" + System.getProperty("file.separator") + "monitor-beans.xml";
+	private String getConfigurationFileName() throws IOException {
+		return getProjectRoot() + System.getProperty("file.separator")
+				+ "configuration" + System.getProperty("file.separator")
+				+ "proxies.xml";
 	}
 
 	private String getProjectRoot() throws IOException {
-		return new File(FileLocator.resolve(CoreActivator.getDefault().getBundle().getEntry("/")).getPath()).getParentFile().getParentFile().getPath();
+		return new File(FileLocator.resolve(this.getBundle().getEntry("/"))
+				.getPath()).getParentFile().getParentFile().getPath();
 	}
-	
+
 	public void stop(BundleContext context) throws Exception {
-		plugin = null;
 		Platform.removeLogListener(logListener);
 		logListener = null;
 		super.stop(context);
-	}
-
-	
-	public static CoreActivator getDefault() {
-		return plugin;
 	}
 
 }
