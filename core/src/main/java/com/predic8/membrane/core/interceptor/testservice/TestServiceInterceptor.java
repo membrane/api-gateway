24c1f70da96b6768c1383c5249358222ef95c4af
Tobias Polley <mail@tobias-polley.de>
2013-10-30 09:40:38 +0100

    removed unused import

--- core/src/main/java/com/predic8/membrane/core/interceptor/testservice/TestServiceInterceptor.java
+++ core/src/main/java/com/predic8/membrane/core/interceptor/testservice/TestServiceInterceptor.java
@@ -23,7 +23,6 @@ import javax.xml.parsers.DocumentBuilderFactory;
 
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
-import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
 import org.w3c.dom.Text;
 import org.xml.sax.SAXParseException;
