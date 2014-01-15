d484661f477e07685e9955f58940c1776c575eae
Tobias Polley <mail@tobias-polley.de>
2014-01-15 12:38:24 +0100

    added license headers

--- osgi-extender/src/main/java/com/predic8/membrane/osgi/extender/OsgiAppender.java
+++ osgi-extender/src/main/java/com/predic8/membrane/osgi/extender/OsgiAppender.java
@@ -1,5 +1,18 @@
-package com.predic8.membrane.osgi.extender;
+/* Copyright 2014 predic8 GmbH, www.predic8.com
 
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+   http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License. */
+
+package com.predic8.membrane.osgi.extender;
 
 import org.apache.log4j.AppenderSkeleton;
 import org.apache.log4j.Level;
@@ -7,6 +20,9 @@ import org.apache.log4j.spi.LoggingEvent;
 import org.apache.log4j.spi.ThrowableInformation;
 import org.osgi.service.log.LogService;
 
+/**
+ * Adapts Membrane's log4j to the logging of the OSGi container.
+ */
 public class OsgiAppender extends AppenderSkeleton {
 
 	private static LogService logService;
