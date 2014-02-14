b5e081cab4446d1a1e09c48cd3bd80426f3ae593
Tobias Polley <mail@tobias-polley.de>
2014-02-14 12:08:35 +0100

    added UnavailableSoapProxyTest case where child bean initialization fails

--- core/src/test/java/com/predic8/membrane/core/rules/UnavailableSoapProxyTest.java
+++ core/src/test/java/com/predic8/membrane/core/rules/UnavailableSoapProxyTest.java
@@ -11,15 +11,17 @@ import org.junit.Before;
 import org.junit.Test;
 
 import com.predic8.membrane.core.Router;
+import com.predic8.membrane.core.interceptor.schemavalidation.ValidatorInterceptor;
 import com.predic8.membrane.core.transport.http.client.HttpClientConfiguration;
 
 public class UnavailableSoapProxyTest {
 
 	private Router r, r2;
+	private SOAPProxy sp;
 
 	@Before
 	public void setup() {
-		SOAPProxy sp = new SOAPProxy();
+		sp = new SOAPProxy();
 		sp.setPort(2000);
 		sp.setWsdl("http://localhost:2001/axis2/services/BLZService?wsdl");
 		r = new Router();
@@ -52,6 +54,12 @@ public class UnavailableSoapProxyTest {
 		rules = r.getRuleManager().getRules();
 		assertEquals(1, rules.size());
 		assertTrue(rules.get(0).isActive());
+	}
+
+	@Test
+	public void doit2() {
+		sp.getInterceptors().add(new ValidatorInterceptor());
+		doit();
 	}
 	
 	@After
