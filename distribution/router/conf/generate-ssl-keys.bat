9a3d32132865444e651b781a13c017f269c16c9a
Tobias Polley <mail@tobias-polley.de>
2015-08-04 15:57:02 +0200

    fixed generate-ssl-keys.bat to generate certificate which are up to date with modern security standards

--- distribution/router/conf/generate-ssl-keys.bat
+++ distribution/router/conf/generate-ssl-keys.bat
@@ -1,7 +1,7 @@
 REM  Script that generates sample keystores for SSL/TLS
 
 REM ---------------------- Client ----------------------------------------------
-keytool -genkey -alias client -keypass secret -keystore client.jks  -storepass secret -dname "cn=client" 
+keytool -genkey -keyalg RSA -keysize 4096 -sigalg SHA256withRSA -alias client -keypass secret -keystore client.jks  -storepass secret -dname "cn=client"
 
 keytool -selfcert -alias client -keystore client.jks -storepass secret -keypass secret -validity 3650
 
@@ -9,7 +9,7 @@ keytool -export -alias client -file client.cer -keystore client.jks -storepass s
 
 REM ---------------------- Membrane ---------------------------------------------
 
-keytool -genkey -alias membrane -keypass secret -keystore membrane.jks  -storepass secret -dname "cn=membrane" 
+keytool -genkey -keyalg RSA -keysize 4096 -sigalg SHA256withRSA -alias membrane -keypass secret -keystore membrane.jks  -storepass secret -dname "cn=membrane"
 
 keytool -selfcert -alias membrane -keystore membrane.jks -storepass secret -keypass secret -validity 3650
 
