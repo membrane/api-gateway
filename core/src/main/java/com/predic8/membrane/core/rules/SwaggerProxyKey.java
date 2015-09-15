a99000e1bf77f9f9d534704f3b8550a2dae1b3f2
Lennart Buhl <git@hackmate.de>
2015-09-15 13:24:03 +0200

    pathTemplateMatch implemented: Path Template Matching for Swagger Urls

--- core/src/main/java/com/predic8/membrane/core/rules/SwaggerProxyKey.java
+++ core/src/main/java/com/predic8/membrane/core/rules/SwaggerProxyKey.java
@@ -1,12 +1,30 @@
+/* Copyright 2009, 2011, 2015 predic8 GmbH, www.predic8.com
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+   http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License. */
 package com.predic8.membrane.core.rules;
 
 import java.util.Map;
 import java.util.Map.Entry;
+import java.util.regex.Pattern;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 
 import io.swagger.models.Path;
 import io.swagger.models.Swagger;
 
 public class SwaggerProxyKey extends ServiceProxyKey {
+	private static Log log = LogFactory.getLog(SwaggerProxyKey.class.getName());
 
 	private Swagger swagger;
 
@@ -28,39 +46,33 @@ public class SwaggerProxyKey extends ServiceProxyKey {
 
 	@Override
 	public boolean complexMatch(String hostHeader, String method, String uri, String version, int port, String localIP) {
-
-		System.out.println("SwaggerProxyKey.complexMatch()...");
-
-		assert swagger != null;
-
+		if (swagger == null) {
+			log.error("Swagger specification is null!");
+			return false;
+		}
+		// check if request is in Swagger specification
 		Map<String, Path> paths = swagger.getPaths();
-		// TODO: check if request is in Swagger specification
 		for (Entry<String,Path> p : paths.entrySet()) {
-
-			String name = p.getKey();
-			Path path = p.getValue();
-
-			System.out.println(name +" "+ path.getParameters() +" "+ method +" "+ uri);
-
-			if (pathTemplateMatch(uri, name) && methodMatch(method, path)) {
+			if (pathTemplateMatch(uri, p.getKey()) && methodMatch(method, p.getValue())) {
+				log.debug("Request is a Swagger call according to specification");
 				return true;
 			}
-
 		}
-
-		//return true; // <-- temporary
-		return false; // <-- when above check works
+		return false;
 	}
 
-	// TODO: Path Template Matching !!!!!!!!!!
+	// Self-made Path Template Matching
 	private boolean pathTemplateMatch(String calledURI, String specName) {
-		// TODO: FIX UGLY TEMPORARY WORKAROUND !!!
-		return calledURI.equals(swagger.getBasePath() + specName);
+		final String IDENTIFIER = "[A-Za-z_0-9]+";
+		specName = specName.replaceAll("\\{" + IDENTIFIER + "\\}", IDENTIFIER);
+		String spec = swagger.getBasePath() + specName;
+		return Pattern.matches(spec, calledURI);
 	}
 
 	private boolean methodMatch(String method, Path path) {
 		return method.equalsIgnoreCase("GET") && path.getGet() != null
 			|| method.equalsIgnoreCase("POST") && path.getPost() != null
+			|| method.equalsIgnoreCase("HEAD") && path.getHead() != null
 			|| method.equalsIgnoreCase("PUT") && path.getPut() != null
 			|| method.equalsIgnoreCase("DELETE") && path.getDelete() != null;
 	}
