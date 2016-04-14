638a94e1400bb0d5e39409a49a8aaf04c6f42aab
Till Born <born@predic8.de>
2016-04-14 17:07:43 +0200

    issue #245: basic Javascript interceptor using Nashorn

--- core/src/main/java/com/predic8/membrane/core/interceptor/javascript/JavaScriptInterceptor.java
+++ core/src/main/java/com/predic8/membrane/core/interceptor/javascript/JavaScriptInterceptor.java
@@ -13,7 +13,129 @@
 
 package com.predic8.membrane.core.interceptor.javascript;
 
+import com.predic8.membrane.annot.MCElement;
+import com.predic8.membrane.annot.MCTextContent;
+import com.predic8.membrane.core.exchange.Exchange;
+import com.predic8.membrane.core.http.Request;
+import com.predic8.membrane.core.http.Response;
 import com.predic8.membrane.core.interceptor.AbstractInterceptor;
+import com.predic8.membrane.core.interceptor.Outcome;
+import com.predic8.membrane.core.util.TextUtil;
+import org.apache.commons.lang.StringEscapeUtils;
 
+import javax.script.ScriptEngine;
+import javax.script.ScriptEngineManager;
+import javax.script.ScriptException;
+import java.util.HashMap;
+
+@MCElement(name = "javascript", mixed = true)
 public class JavaScriptInterceptor extends AbstractInterceptor {
+    private String src = "";
+    private String modifiedScript;
+
+    ScriptEngine engine = new ScriptEngineManager().getEngineByName("nashorn");
+
+    public JavaScriptInterceptor(){
+        name = "Javascript";
+    }
+
+
+    @Override
+    public void init() throws Exception {
+        if (router == null)
+            return;
+        if ("".equals(src))
+            return;
+
+        modifiedScript = getScriptWithImports();
+    }
+
+    @Override
+    public Outcome handleRequest(Exchange exc) throws Exception {
+        return runScript(exc, Flow.REQUEST);
+    }
+
+    @Override
+    public Outcome handleResponse(Exchange exc) throws Exception {
+        return runScript(exc, Flow.RESPONSE);
+    }
+
+    @Override
+    public void handleAbort(Exchange exc) {
+        try {
+            runScript(exc, Flow.ABORT);
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+        } catch (ScriptException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private Outcome runScript(Exchange exc, Flow flow) throws InterruptedException, ScriptException {
+        addScriptParameters(exc, flow);
+
+        Object res = runScript();
+
+        if (res instanceof Outcome) {
+            return (Outcome) res;
+        }
+
+        if (res instanceof Response) {
+            exc.setResponse((Response) res);
+            return Outcome.RETURN;
+        }
+
+        if (res instanceof Request) {
+            exc.setRequest((Request) res);
+        }
+        return Outcome.CONTINUE;
+    }
+
+    private String getScriptWithImports() {
+        return "var imports = new JavaImporter(com.predic8.membrane.core.interceptor.Outcome," +
+                "com.predic8.membrane.core.http" +
+                ")\n" +
+                "with(imports){\n" +
+                        getSrc() +
+                        "\n}";
+    }
+
+    private Object runScript() throws ScriptException {
+        return engine.eval(modifiedScript);
+    }
+
+    private void addScriptParameters(Exchange exc, Flow flow) {
+        HashMap<String, Object> parameters = new HashMap<String, Object>();
+        parameters.put("exc", exc);
+        parameters.put("flow", flow);
+        parameters.put("spring", router.getBeanFactory());
+
+        for(String name : parameters.keySet())
+            engine.put(name,parameters.get(name));
+    }
+
+
+    public String getSrc() {
+        return src;
+    }
+
+    @MCTextContent
+    public void setSrc(String src) {
+        this.src = src;
+    }
+
+    @Override
+    public String getShortDescription() {
+        return "Executes a Javascript script.";
+    }
+
+    @Override
+    public String getLongDescription() {
+        StringBuilder sb = new StringBuilder();
+        sb.append(TextUtil.removeFinalChar(getShortDescription()));
+        sb.append(":<br/><pre style=\"overflow-x:auto\">");
+        sb.append(StringEscapeUtils.escapeHtml(TextUtil.removeCommonLeadingIndentation(src)));
+        sb.append("</pre>");
+        return sb.toString();
+    }
 }
