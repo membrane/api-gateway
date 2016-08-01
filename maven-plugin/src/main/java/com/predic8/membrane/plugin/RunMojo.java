da2fffbaf7ac7963cb7b907be50d58c80f09a55d
Oliver Weiler <wo6980@gmail.com>
2016-08-01 13:37:02 +0200

    Update RunMojo.java

--- maven-plugin/src/main/java/com/predic8/membrane/plugin/RunMojo.java
+++ maven-plugin/src/main/java/com/predic8/membrane/plugin/RunMojo.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2016 helpermethod
+ * Copyright 2016 predic8 GmbH
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
