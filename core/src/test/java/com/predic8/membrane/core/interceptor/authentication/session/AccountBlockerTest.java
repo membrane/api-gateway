34f5740dda4b204406575db8b719ac972eee5fbe
Tobias Polley <mail@tobias-polley.de>
2016-12-16 13:20:12 +0100

    test for account blocker

---
+++ core/src/test/java/com/predic8/membrane/core/interceptor/authentication/session/AccountBlockerTest.java
@@ -0,0 +1,34 @@
+package com.predic8.membrane.core.interceptor.authentication.session;
+
+import org.junit.Test;
+import org.springframework.util.Assert;
+
+/**
+ * Created by Tobias on 12/16/2016.
+ */
+public class AccountBlockerTest {
+    @Test
+    public void run() throws InterruptedException {
+        AccountBlocker ab = new AccountBlocker();
+        ab.setBlockFor(1000);
+        ab.setAfterFailedLoginsWithin(1000);
+        ab.setAfterFailedLogins(9);
+
+        for (int i = 1; i <= 10 ; i++) {
+            Assert.isTrue(!ab.isBlocked("foo"), "login should not be blocked in loop " + i);
+            ab.fail("foo");
+        }
+        Assert.isTrue(ab.isBlocked("foo"));
+
+        Thread.sleep(2000);
+
+        for (int i = 1; i <= 10 ; i++) {
+            Assert.isTrue(!ab.isBlocked("foo"), "login should not be blocked in loop " + i);
+            ab.fail("foo");
+        }
+        Assert.isTrue(ab.isBlocked("foo"));
+
+    }
+
+
+}
