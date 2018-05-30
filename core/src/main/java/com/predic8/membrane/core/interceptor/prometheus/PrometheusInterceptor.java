1a4b51021b0e29f4b2f80a08ae2336605c135fc1
Tobias Polley <mail@tobias-polley.de>
2018-05-30 16:43:43 +0200

    fixed prometheus compatible data format

--- core/src/main/java/com/predic8/membrane/core/interceptor/prometheus/PrometheusInterceptor.java
+++ core/src/main/java/com/predic8/membrane/core/interceptor/prometheus/PrometheusInterceptor.java
@@ -25,56 +25,125 @@ import com.predic8.membrane.core.interceptor.AbstractInterceptor;
 import com.predic8.membrane.core.interceptor.Outcome;
 import com.predic8.membrane.core.rules.Rule;
 import com.predic8.membrane.core.rules.StatisticCollector;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
+import java.util.HashSet;
 import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 import java.util.regex.Pattern;
 
 @MCElement(name = "prometheus")
 public class PrometheusInterceptor extends AbstractInterceptor {
 
+    static final Logger LOG = LoggerFactory.getLogger(PrometheusInterceptor.class);
+    static volatile boolean issuedDuplicateRuleNameWarning = false;
 
     @Override
     public Outcome handleRequest(Exchange exc) throws Exception {
-        StringBuilder sb = new StringBuilder();
-        buildPrometheusStyleResponse(sb);
-        exc.setResponse(Response.ok(sb.toString()).header(Header.CONTENT_TYPE, MimeType.TEXT_PLAIN_UTF8).build());
+        Context ctx = new Context();
+        buildPrometheusStyleResponse(ctx);
+        exc.setResponse(Response.ok(ctx.sb.toString()).header(Header.CONTENT_TYPE, "text/plain; version=0.0.4").build());
         return Outcome.RETURN;
     }
 
-    private void buildPrometheusStyleResponse(StringBuilder sb) {
-        for (Rule r : router.getRuleManager().getRules())
-            buildStatuscodeLines(sb, r);
-    }
+    private class Context {
+        StringBuilder sb = new StringBuilder();
 
-    private StringBuilder buildStatuscodeLines(StringBuilder sb, Rule rule) {
-        Map<Integer, StatisticCollector> stats = rule.getStatisticsByStatusCodes();
+        StringBuilder s1 = new StringBuilder();
+        StringBuilder s2 = new StringBuilder();
+        StringBuilder s3 = new StringBuilder();
+        StringBuilder s4 = new StringBuilder();
+        StringBuilder s5 = new StringBuilder();
 
-        for (Integer code : stats.keySet()) {
-            buildLine(sb, rule.getName(), String.valueOf(stats.get(code).getCount()), "code", code, "count");
-            buildLine(sb, rule.getName(), String.valueOf(stats.get(code).getMinTime()), "code", code, "min");
-            buildLine(sb, rule.getName(), String.valueOf(stats.get(code).getMaxTime()), "code", code, "max");
-            buildLine(sb, rule.getName(), String.valueOf(stats.get(code).getAvgTime()), "code", code, "avg");
+        HashSet<String> seenRules = new HashSet<>();
+
+        private void reset() {
+            s1.setLength(0);
+            s2.setLength(0);
+            s3.setLength(0);
+            s4.setLength(0);
+            s5.setLength(0);
         }
 
+        private void resetAll() {
+            sb.setLength(0);
+            seenRules.clear();
+        }
+    }
+
+    private void buildPrometheusStyleResponse(Context ctx) {
+        ctx.resetAll();
+        for (Rule r : router.getRuleManager().getRules()) {
+            if (!ctx.seenRules.add(r.getName())) {
+                // the prometheus format is not allowed to contain the same metric more than once
+                if (issuedDuplicateRuleNameWarning)
+                    continue;
+                LOG.warn("The prometheus interceptor detected the same rule name more than once.");
+                issuedDuplicateRuleNameWarning = true;
+                continue;
+            }
+
+            buildStatuscodeLines(ctx, r);
+        }
+    }
+
+    private void buildStatuscodeLines(Context ctx, Rule rule) {
+        Map<Integer, StatisticCollector> stats = rule.getStatisticCollector().getStatisticsByStatusCodes();
+
+        ctx.reset();
+
+        for (Integer code : stats.keySet()) {
+            buildLine(ctx.s1, rule.getName(), stats.get(code).getCount(), "code", code, "count");
+            buildLine(ctx.s2, rule.getName(), stats.get(code).getGoodCount(), "code", code, "good_count");
+            buildLine(ctx.s3, rule.getName(), stats.get(code).getGoodTotalTime(), "code", code, "good_time");
+            buildLine(ctx.s4, rule.getName(), stats.get(code).getGoodTotalBytesSent(), "code", code, "good_bytes_req_body");
+            buildLine(ctx.s5, rule.getName(), stats.get(code).getGoodTotalBytesReceived(), "code", code, "good_bytes_res_body");
+        }
+
+        ctx.sb.append(ctx.s1);
+        ctx.sb.append(ctx.s2);
+        ctx.sb.append(ctx.s3);
+        ctx.sb.append(ctx.s4);
+        ctx.sb.append(ctx.s5);
+    }
+
+    /**
+     * see https://prometheus.io/docs/instrumenting/exposition_formats/ .
+     */
+    private StringBuilder buildLine(StringBuilder sb, String ruleName, long value, String labelName, int labelValue, String postFix) {
+        String prometheusName = prometheusCompatibleName(ruleName + "_" + postFix);
+        if (sb.length() == 0) {
+            sb.append("# TYPE " + prometheusName + " counter\n");
+        }
+        sb.append(prometheusName);
+        sb.append("{");
+        sb.append(prometheusCompatibleName(labelName));
+        sb.append("=\"");
+        sb.append(labelValue);
+        sb.append("\"} ");
+        sb.append(value);
+        sb.append("\n");
         return sb;
     }
 
-    private StringBuilder buildLine(StringBuilder sb, String ruleName, Object value, Object... keys) {
-        sb.append(toLowerCaseUnderscore(replaceColonWithUnknown(ruleName)));
-        for (Object key : keys)
-            sb.append("_").append(String.valueOf(key));
-        sb.append(" ").append(String.valueOf(value)).append(System.lineSeparator());
+    ConcurrentHashMap<String, String> names = new ConcurrentHashMap<>();
+    Pattern ILLEGAL_CHARS = Pattern.compile("[ :\n\r\"\\\\]");
 
-        return sb;
+    private String prometheusCompatibleName(String name) {
+        String result = names.get(name);
+        if (result != null)
+            return result;
+
+        result = prettyPrint(name);
+
+        result = ILLEGAL_CHARS.matcher(result).replaceAll("_");
+        names.put(name, result);
+
+        return result;
     }
 
-    private String replaceColonWithUnknown(String ruleName) {
-        if (ruleName.startsWith(":"))
-            return ruleName.replaceFirst(Pattern.quote(":"), "unknown_");
-        return ruleName;
-    }
-
-    private String toLowerCaseUnderscore(String ruleName) {
+    private String prettyPrint(String ruleName) {
         StringBuilder sb = new StringBuilder();
         String[] split = ruleName.toLowerCase().split(" ");
         for (String s : split)
