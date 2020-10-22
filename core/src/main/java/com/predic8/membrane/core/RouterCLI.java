0e03ee7f2315e65f8a74a24d80d16aea9c9692e4
Mehmet Can Cömert <mehmet.coemert@kisters.de>
2020-10-22 17:17:22 +0200

    Fix #357 by converting path to URI first.

--- core/src/main/java/com/predic8/membrane/core/RouterCLI.java
+++ core/src/main/java/com/predic8/membrane/core/RouterCLI.java
@@ -14,6 +14,8 @@
 
 package com.predic8.membrane.core;
 
+import java.io.File;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.xml.XmlBeanDefinitionStoreException;
@@ -116,9 +118,15 @@ public class RouterCLI {
 	}
 
 	private static String prefix(String dir) {
-		if (dir.startsWith("/"))
-			return "file://" + dir;
-		return dir;
+
+		String result = dir;
+
+		File file = new File(dir);
+		if (file.isAbsolute()) {
+			result = file.toURI().toString();
+		}
+
+		return result;
 	}
 
 }
