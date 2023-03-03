9c6b52bef5aabe4d3724af3066156af716bd550c
Thomas Bayer <bayer@predic8.de>
2023-03-03 20:38:39 +0100

    German Umlaut

--- core/src/test/java/com/predic8/membrane/core/interceptor/jwt/JwtAuthInterceptorTest.java
+++ core/src/test/java/com/predic8/membrane/core/interceptor/jwt/JwtAuthInterceptorTest.java
@@ -41,7 +41,7 @@ import static org.mockito.Mockito.when;
 public class JwtAuthInterceptorTest{
 
     public static final String KID = "membrane";
-    public static final String SUB_CLAIM_CONTENT = "Till, der flei�ige Programmierer";
+    public static final String SUB_CLAIM_CONTENT = "Till, der fleissige Programmierer";
     private static final String AUDIENCE = "AusgestelltFuer";
 
     public static Collection<Object[]> data() throws Exception {
