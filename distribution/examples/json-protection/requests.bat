d0d1ff498b63a9f2f5f774db761143c3736c17a7
Torben Burchgart <burchgart@predic8.de>
2023-06-07 17:32:36 +0200

    Replaced python based JSON beautifier with beautifier plugin

--- distribution/examples/json-protection/requests.bat
+++ distribution/examples/json-protection/requests.bat
@@ -2,48 +2,48 @@
 
 echo --------- Valid Request ---------
 echo.
-curl -d @requests/valid.json localhost:2000 | python -m json.tool
+curl -d @requests/valid.json localhost:2000
 echo ---------------------------------
 echo.
 
 echo -------- Too Many Tokens --------
 echo.
-curl -d @requests/max_tokens.json localhost:2000 | python -m json.tool
+curl -d @requests/max_tokens.json localhost:2000
 echo ---------------------------------
 echo.
 
 echo ------- Document Too Large ------
 echo.
-curl -d @requests/max_size.json localhost:2000 | python -m json.tool
+curl -d @requests/max_size.json localhost:2000
 echo ---------------------------------
 echo.
 
 echo -------- Nesting Too Deep -------
 echo.
-curl -d @requests/max_depth.json localhost:2000 | python -m json.tool
+curl -d @requests/max_depth.json localhost:2000
 echo ---------------------------------
 echo.
 
 echo -------- String Too Large -------
 echo.
-curl -d @requests/max_string_length.json localhost:2000 | python -m json.tool
+curl -d @requests/max_string_length.json localhost:2000
 echo ---------------------------------
 echo.
 
 echo --------- Key Too Large ---------
 echo.
-curl -d @requests/max_key_length.json localhost:2000 | python -m json.tool
+curl -d @requests/max_key_length.json localhost:2000
 echo ---------------------------------
 echo.
 
 echo -------- Object Too Large -------
 echo.
-curl -d @requests/max_object_size.json localhost:2000 | python -m json.tool
+curl -d @requests/max_object_size.json localhost:2000
 echo ---------------------------------
 echo.
 
 echo -------- Array Too Large --------
 echo.
-curl -d @requests/max_array_size.json localhost:2000 | python -m json.tool
+curl -d @requests/max_array_size.json localhost:2000
 echo ---------------------------------
 echo.
