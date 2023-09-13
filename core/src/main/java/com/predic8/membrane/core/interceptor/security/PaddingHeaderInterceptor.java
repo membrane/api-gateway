4fed9e2e6b12c418b330a19895e37999fe1cc9bf
Christian Gördes <goerdes@predic8.de>
2023-09-13 10:50:22 +0200

    added package

--- core/src/main/java/com/predic8/membrane/core/interceptor/security/PaddingHeaderInterceptor.java
+++ core/src/main/java/com/predic8/membrane/core/interceptor/security/PaddingHeaderInterceptor.java
@@ -1,10 +1,12 @@
-package com.predic8.membrane.core.interceptor;
+package com.predic8.membrane.core.interceptor.security;
 
 import com.predic8.membrane.annot.MCElement;
 import com.predic8.membrane.core.exchange.Exchange;
+import com.predic8.membrane.core.interceptor.AbstractInterceptor;
+import com.predic8.membrane.core.interceptor.Outcome;
 
 @MCElement(name = "paddingHeader")
-public class PaddingHeaderInterceptor extends AbstractInterceptor{
+public class PaddingHeaderInterceptor extends AbstractInterceptor {
     @Override
     public Outcome handleRequest(Exchange exc) throws Exception {
         return super.handleRequest(exc);
