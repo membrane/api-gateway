c13552d0a6980ab91b58aa1c3ef31006150bc7f4
Inphinitation <47746962+Inphinitation@users.noreply.github.com>
2023-10-23 11:13:22 +0200

    Migrated from JKS crypto stores to PKCS#12

--- distribution/router/conf/generate-ssl-keys.bat
+++ distribution/router/conf/generate-ssl-keys.bat
@@ -1,23 +1,23 @@
 REM  Script that generates sample keystores for SSL/TLS
 
 REM ---------------------- Client ----------------------------------------------
-keytool -genkey -keyalg RSA -keysize 4096 -sigalg SHA256withRSA -alias client -keypass secret -keystore client.jks  -storepass secret -dname "cn=client"
+keytool -genkey -keyalg RSA -keysize 4096 -sigalg SHA256withRSA -storetype PKCS12 -alias client -keypass secret -keystore client.p12 -storepass secret -dname "cn=client"
 
-keytool -selfcert -alias client -keystore client.jks -storepass secret -keypass secret -validity 3650
+keytool -selfcert -alias client -keystore client.p12 -storetype PKCS12 -storepass secret -keypass secret -validity 3650
 
-keytool -export -alias client -file client.cer -keystore client.jks -storepass secret
+keytool -export -alias client -file client.cer -keystore client.p12 -storetype PKCS12 -storepass secret
 
 REM ---------------------- Membrane ---------------------------------------------
 
-keytool -genkey -keyalg RSA -keysize 4096 -sigalg SHA256withRSA -alias membrane -keypass secret -keystore membrane.jks  -storepass secret -dname "cn=membrane"
+keytool -genkey -keyalg RSA -keysize 4096 -sigalg SHA256withRSA -alias membrane -keypass secret -keystore membrane.p12 -storetype PKCS12 -storepass secret -dname "cn=membrane"
 
-keytool -selfcert -alias membrane -keystore membrane.jks -storepass secret -keypass secret -validity 3650
+keytool -selfcert -alias membrane -keystore membrane.p12 -storetype PKCS12 -storepass secret -keypass secret -validity 3650
 
-keytool -export -alias membrane -file membrane.cer -keystore membrane.jks -storepass secret
+keytool -export -alias membrane -file membrane.cer -keystore membrane.p12 -storetype PKCS12 -storepass secret
 
 
 REM --------------------  Exchange of certificates -------------------------------
 
-keytool -import -alias membrane -file membrane.cer -keystore client.jks -storepass secret
+keytool -import -alias membrane -file membrane.cer -keystore client.p12 -storetype PKCS12 -storepass secret
 
-keytool -import -alias client -file client.cer -keystore membrane.jks -storepass secret
+keytool -import -alias client -file client.cer -keystore membrane.p12 -storetype PKCS12 -storepass secret
