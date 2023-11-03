5a6cf290ab39b641ae3f0ef068260f4726031c29
Kai Klemmer <klemmer@predic8.de>
2023-11-03 15:02:32 +0100

    refactor test

--- distribution/src/test/java/com/predic8/membrane/examples/tests/OpenTelemetryInterceptorTest.java
+++ distribution/src/test/java/com/predic8/membrane/examples/tests/OpenTelemetryInterceptorTest.java
@@ -4,16 +4,16 @@ package com.predic8.membrane.examples.tests;
 import com.predic8.membrane.examples.util.AbstractSampleMembraneStartStopTestcase;
 import com.predic8.membrane.examples.util.BufferLogger;
 import com.predic8.membrane.examples.util.Process2;
-import io.restassured.filter.log.RequestLoggingFilter;
-import io.restassured.filter.log.ResponseLoggingFilter;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.regex.Matcher;
 
-import static com.predic8.membrane.test.AssertUtils.assertContains;
-import static io.restassured.RestAssured.filters;
 import static io.restassured.RestAssured.given;
+import static java.util.regex.Pattern.compile;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class OpenTelemetryInterceptorTest extends AbstractSampleMembraneStartStopTestcase {
 
@@ -29,8 +29,6 @@ public class OpenTelemetryInterceptorTest extends AbstractSampleMembraneStartSto
         logger = new BufferLogger();
         process = new Process2.Builder().in(baseDir).script("service-proxy").withWatcher(logger).waitForMembrane().start();
 
-        // Dump HTTP
-        filters(new RequestLoggingFilter(), new ResponseLoggingFilter());
     }
 
 
@@ -41,7 +39,18 @@ public class OpenTelemetryInterceptorTest extends AbstractSampleMembraneStartSto
                 .then().assertThat()
                 .statusCode(200);
 
-        assertContains("Request headers:", logger.toString());
-        assertContains("traceparent", logger.toString());
+        Matcher m = compile("traceparent:\\s+\\S+").matcher(logger.toString());
+        ArrayList<String> traces = new ArrayList<>();
+        while (m.find()) {
+            // split string at - and get traceId
+            traces.add(m.group(0).trim().split("-")[1]);
+        }
+
+        // check if traceparent appears twice in the header
+        assertEquals(2, traces.size());
+
+        // check if traceId matches:
+        assertEquals(traces.get(0), traces.get(1));
+
     }
 }
