045142644c79f4a35151e71d79ec0aec5c933a2d
predic8 <burchgart@predic8.de>
2024-03-07 14:47:10 +0100

    Fetch openapi specs

--- core/src/main/java/com/predic8/membrane/core/interceptor/apidocs/ApiDocsInterceptor.java
+++ core/src/main/java/com/predic8/membrane/core/interceptor/apidocs/ApiDocsInterceptor.java
@@ -5,17 +5,18 @@ import com.predic8.membrane.core.exchange.Exchange;
 import com.predic8.membrane.core.interceptor.AbstractInterceptor;
 import com.predic8.membrane.core.interceptor.Interceptor;
 import com.predic8.membrane.core.interceptor.Outcome;
+import com.predic8.membrane.core.openapi.serviceproxy.APIProxy;
 import com.predic8.membrane.core.openapi.serviceproxy.OpenAPIInterceptor;
+import com.predic8.membrane.core.openapi.serviceproxy.OpenAPISpec;
 
 import java.util.List;
-import java.util.stream.Collectors;
 
 import static com.predic8.membrane.core.interceptor.Outcome.CONTINUE;
 
 @MCElement(name = "apiDocs")
 public class ApiDocsInterceptor extends AbstractInterceptor {
 
-    private List<Interceptor> apis;
+    private List<List<OpenAPISpec>> apis;
     private boolean initialized = false;
 
     public ApiDocsInterceptor() {
@@ -33,10 +34,12 @@ public class ApiDocsInterceptor extends AbstractInterceptor {
         return CONTINUE;
     }
 
-    private List<Interceptor> getApis() {
+    private List<List<OpenAPISpec>> getApis() {
         var i = router.getRuleManager().getRules().stream()
                 .flatMap(rule -> rule.getInterceptors().stream())
-                .filter(ic -> ic instanceof OpenAPIInterceptor).toList();
+                .filter(ic -> ic instanceof OpenAPIInterceptor)
+                .map(icept -> ((OpenAPIInterceptor) icept).getApiProxy())
+                .map(APIProxy::getSpecs).toList();
 
         return i;
     }
