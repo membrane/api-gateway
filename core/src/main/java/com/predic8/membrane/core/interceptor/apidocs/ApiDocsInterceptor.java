f39854e07d7c55ba6baffb2f68479cd2751771a8
predic8 <burchgart@predic8.de>
2024-03-07 11:18:03 +0100

    Init add apidocs interceptor

---
+++ core/src/main/java/com/predic8/membrane/core/interceptor/apidocs/ApiDocsInterceptor.java
@@ -0,0 +1,14 @@
+package com.predic8.membrane.core.interceptor.apidocs;
+
+import com.predic8.membrane.annot.MCElement;
+import com.predic8.membrane.core.interceptor.AbstractInterceptor;
+
+@MCElement(name = "apiDocs")
+public class ApiDocsInterceptor extends AbstractInterceptor {
+
+    public ApiDocsInterceptor() {
+        name = "Api Docs";
+    }
+
+
+}
---
+++ core/src/main/java/com/predic8/membrane/core/interceptor/apidocs/ApiRegistry.java
@@ -0,0 +1,8 @@
+package com.predic8.membrane.core.interceptor.apidocs;
+
+import com.predic8.membrane.annot.MCElement;
+
+@MCElement(name = "apiRegistry")
+public class ApiRegistry {
+
+}
