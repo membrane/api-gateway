81f15ebdf2bc3a2c1831428b5518c26b19803607
Christian Gördes <goerdes@predic8.de>
2024-03-14 14:21:36 +0100

    Added license headers

--- core/src/main/java/com/predic8/membrane/core/interceptor/log/AccessLogInterceptor.java
+++ core/src/main/java/com/predic8/membrane/core/interceptor/log/AccessLogInterceptor.java
@@ -12,7 +12,6 @@
    See the License for the specific language governing permissions and
    limitations under the License. */
 
-
 package com.predic8.membrane.core.interceptor.log;
 
 import com.predic8.membrane.annot.MCAttribute;
