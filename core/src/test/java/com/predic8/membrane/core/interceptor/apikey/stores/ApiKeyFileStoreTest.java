eeaeab2c53cf797cfb4ec25d0c8aa1dbb4976819
Christian Gördes <goerdes@predic8.de>
2024-04-25 16:07:55 +0200

    added missing import

--- core/src/test/java/com/predic8/membrane/core/interceptor/apikey/stores/ApiKeyFileStoreTest.java
+++ core/src/test/java/com/predic8/membrane/core/interceptor/apikey/stores/ApiKeyFileStoreTest.java
@@ -13,6 +13,7 @@
    limitations under the License. */
 package com.predic8.membrane.core.interceptor.apikey.stores;
 
+import com.predic8.membrane.core.Router;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
