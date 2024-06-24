e4ae22ede65cc3519fe210e8dbd405747aac709a
Torben Burchgart <burchgart@predic8.de>
2024-06-24 16:15:33 +0200

    Made flow return optional in SetHeaderInterceptord

--- core/src/main/java/com/predic8/membrane/core/interceptor/misc/SetHeaderInterceptor.java
+++ core/src/main/java/com/predic8/membrane/core/interceptor/misc/SetHeaderInterceptor.java
@@ -4,24 +4,32 @@ import com.predic8.membrane.annot.MCElement;
 import com.predic8.membrane.core.exchange.Exchange;
 import com.predic8.membrane.core.http.Message;
 
+import java.util.Optional;
+
 @MCElement(name = "setHeader")
 public class SetHeaderInterceptor extends AbstractSetterInterceptor {
 
     @Override
     protected boolean shouldSetValue(Exchange exc, Flow flow) {
         if (ifAbsent) {
-            return !msgFromExchange(exc).getHeader().contains(name);
+            var msg = msgFromExchange(exc, flow);
+            return msg.filter(message -> !message.getHeader().contains(name)).isPresent();
         }
         return true;
     }
 
     @Override
     protected void setValue(Exchange exc, Flow flow, String eval) {
-        msgFromExchange(exc).getHeader().setValue(name, eval);
+        var msg = msgFromExchange(exc, flow);
+        msg.ifPresent(message -> message.getHeader().setValue(name, eval));
     }
 
-    private Message msgFromExchange(Exchange exc) {
-        return exc.getRequest() != null ? exc.getRequest() : exc.getResponse();
+    private Optional<Message> msgFromExchange(Exchange exc, Flow flow) {
+        return switch (flow) {
+            case REQUEST -> Optional.of(exc.getRequest());
+            case RESPONSE -> Optional.of(exc.getResponse());
+            case ABORT -> Optional.empty();
+        };
     }
 
     @Override
