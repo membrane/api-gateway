0b82aa9770d3005fca06ee455269c4d6694727e2
Christian Gördes <118011644+christiangoerdes@users.noreply.github.com>
2024-07-05 11:38:43 +0200

    Fixed paths and termination errors of startup scripts (#1166)
    * Fixed paths and termination errors of startup scripts
    
    * fix
    
    ---------
    
    Co-authored-by: t-burch <119930761+t-burch@users.noreply.github.com>

--- distribution/examples/loadbalancing/5-multiple/service-proxy.sh
+++ distribution/examples/loadbalancing/5-multiple/service-proxy.sh
@@ -11,7 +11,7 @@ homeSet() {
 
 terminate() {
 	echo "Starting of Membrane Router failed."
-	echo "Please execute this script from the MEMBRANE_HOME/examples/loadbalancer-multiple-4 directory"
+	echo "Please execute this script from the MEMBRANE_HOME/examples/loadbalancing/5-multiple directory"
 	
 }
 
