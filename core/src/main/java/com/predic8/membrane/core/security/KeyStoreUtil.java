fa61519a5957cff0c247f8c88cd7f0b05c15900f
Torben Burchgart <burchgart@predic8.de>
2024-08-28 09:26:48 +0200

    Add methods for generating an SHA-256 certificate digest and loading a KeyStore based on provided parameters. Update tests to validate these functionalities, ensuring correct behavior for alias handling. Refactor existing methods for consistency and clarity.

--- core/src/main/java/com/predic8/membrane/core/security/KeyStoreUtil.java
+++ core/src/main/java/com/predic8/membrane/core/security/KeyStoreUtil.java
@@ -1,11 +1,79 @@
+/* Copyright 2024 predic8 GmbH, www.predic8.com
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+   http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License. */
 package com.predic8.membrane.core.security;
 
-import java.security.KeyStore;
-import java.security.KeyStoreException;
+import com.predic8.membrane.core.config.security.Store;
+import com.predic8.membrane.core.resolver.ResolverMap;
+import org.jetbrains.annotations.NotNull;
+
+import java.io.IOException;
+import java.security.*;
+import java.security.cert.CertificateEncodingException;
+import java.security.cert.CertificateException;
 import java.util.Enumeration;
 import java.util.Optional;
 
 public class KeyStoreUtil {
+    /**
+     * Generates an SHA-256 digest of the certificate associated with the given alias in the KeyStore.
+     *
+     * @param ks    The KeyStore containing the certificate.
+     * @param alias The alias of the certificate in the KeyStore.
+     * @return A String representation of the SHA-256 digest, with bytes separated by colons.
+     * @throws CertificateEncodingException If there's an error encoding the certificate.
+     * @throws KeyStoreException           If there's an error accessing the KeyStore.
+     * @throws NoSuchAlgorithmException    If the SHA-256 algorithm is not available.
+     */
+    public static @org.jetbrains.annotations.NotNull String getDigest(KeyStore ks, String alias) throws CertificateEncodingException, KeyStoreException, NoSuchAlgorithmException {
+        byte[] pkeEnc = ks.getCertificate(alias).getEncoded();
+        MessageDigest md = MessageDigest.getInstance("SHA-256");
+        md.update(pkeEnc);
+        byte[] mdbytes = md.digest();
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < mdbytes.length; i++) {
+            if (i > 0)
+                sb.append(':');
+            sb.append(Integer.toString((mdbytes[i] & 0xff) + 0x100, 16).substring(1));
+        }
+        return sb.toString();
+    }
+
+    /**
+     * Retrieves and loads a KeyStore based on the provided parameters.
+     *
+     * @param store The Store object containing information about the keystore.
+     * @param resourceResolver The ResolverMap used to resolve the keystore location.
+     * @param baseLocation The base location to be combined with the store's location.
+     * @param type The type of the KeyStore (e.g., "JKS", "PKCS12").
+     * @param password The password used to check the integrity of the keystore.
+     * @return A loaded KeyStore instance.
+     * @throws KeyStoreException If there's an error accessing the keystore.
+     * @throws NoSuchProviderException If the specified provider is not found.
+     * @throws IOException If there's an I/O error while loading the keystore.
+     * @throws NoSuchAlgorithmException If the algorithm used to check the integrity of the keystore cannot be found.
+     * @throws CertificateException If any of the certificates in the keystore could not be loaded.
+     */
+    public static @NotNull KeyStore getAndLoadKeyStore(Store store, ResolverMap resourceResolver, String baseLocation, String type, char[] password) throws KeyStoreException, NoSuchProviderException, IOException, NoSuchAlgorithmException, CertificateException {
+        KeyStore ks;
+        if (store.getProvider() != null)
+            ks = KeyStore.getInstance(type, store.getProvider());
+        else
+            ks = KeyStore.getInstance(type);
+        ks.load(resourceResolver.resolve(ResolverMap.combine(baseLocation, store.getLocation())), password);
+        return ks;
+    }
+
     /**
      * Retrieves the first certificate alias from the provided key store.
      *
--- core/src/test/java/com/predic8/membrane/core/security/KeyStoreUtilTest.java
+++ core/src/test/java/com/predic8/membrane/core/security/KeyStoreUtilTest.java
@@ -1,72 +1,113 @@
+/* Copyright 2024 predic8 GmbH, www.predic8.com
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+   http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License. */
 package com.predic8.membrane.core.security;
 
 import com.predic8.membrane.core.HttpRouter;
 import com.predic8.membrane.core.Router;
-import org.jetbrains.annotations.NotNull;
+import com.predic8.membrane.core.config.security.KeyStore;
+import com.predic8.membrane.core.config.security.SSLParser;
+import com.predic8.membrane.core.resolver.ResolverMap;
+import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
-import java.security.KeyStore;
+import java.io.IOException;
+import java.security.KeyStoreException;
+import java.security.NoSuchAlgorithmException;
+import java.security.NoSuchProviderException;
+import java.security.cert.CertificateEncodingException;
+import java.security.cert.CertificateException;
 import java.util.Optional;
 
-import static com.predic8.membrane.core.transport.ssl.StaticSSLContext.openKeyStore;
 import static org.junit.jupiter.api.Assertions.*;
 
-public class KeyStoreUtilTest {
+class KeyStoreUtilsTest {
 
     private static Router router;
+    private static java.security.KeyStore keyStore;
+    private static final String ALIAS = "key1";
+    private static final String KEYSTORE_PASSWORD = "secret";
+    private static final String EXPECTED_DIGEST = "96:ec:da:3d:2a:a3:a9:7e:3b:40:56:46:86:d7:1c:d2:a9:e1:69:3f:99:6b:8d:57:4a:4c:bb:7a:24:55:18:ed";
 
     @BeforeAll
-    public static void before() {
+    static void setUp() throws Exception {
         router = new HttpRouter();
+        SSLParser sslParser = new SSLParser();
+        sslParser.setKeyStore(new KeyStore());
+        sslParser.getKeyStore().setLocation("classpath:/alias-keystore.p12");
+        sslParser.getKeyStore().setKeyPassword(KEYSTORE_PASSWORD);
+        keyStore = KeyStoreUtil.getAndLoadKeyStore(sslParser.getKeyStore(), router.getResolverMap(), router.getBaseLocation(), "PKCS12", KEYSTORE_PASSWORD.toCharArray());
+    }
+
+    @AfterAll
+    static void tearDown() {
+        router.stop();
     }
 
     @Test
-    public void containsCertWithAlias_PresentAlias() throws Exception {
-        KeyStore keystore = getAliasKeystoreByAlias("key1", router);
-        assertTrue(KeyStoreUtil.containsCertWithAlias(keystore, "key1"));
+    void testGetDigest() throws CertificateEncodingException, KeyStoreException, NoSuchAlgorithmException {
+        String digest = KeyStoreUtil.getDigest(keyStore, ALIAS);
+        assertEquals(EXPECTED_DIGEST, digest);
     }
 
     @Test
-    public void containsCertWithAlias_AbsentAlias() throws Exception {
-        KeyStore keystore = getAliasKeystoreByAlias("key1", router);
-        assertFalse(KeyStoreUtil.containsCertWithAlias(keystore, "key2"));
+    void testGetAndLoadKeyStore() throws KeyStoreException, NoSuchProviderException, IOException, NoSuchAlgorithmException, CertificateException {
+        KeyStore store = new KeyStore();
+        store.setLocation("classpath:/alias-keystore.p12");
+        store.setKeyPassword(KEYSTORE_PASSWORD);
+        java.security.KeyStore loadedKeyStore = KeyStoreUtil.getAndLoadKeyStore(store, router.getResolverMap(), router.getBaseLocation(), "PKCS12", KEYSTORE_PASSWORD.toCharArray());
+        assertNotNull(loadedKeyStore);
+        assertTrue(loadedKeyStore.size() > 0);
+        assertTrue(loadedKeyStore.containsAlias(ALIAS));
     }
 
     @Test
-    public void containsCertWithAlias_EmptyKeystore() throws Exception {
-        KeyStore keystore = getAliasKeystoreByAlias("key3", router);
-        assertFalse(KeyStoreUtil.containsCertWithAlias(keystore, "key3"));
+    void testFirstAliasOrThrowFound() throws KeyStoreException {
+        String alias = KeyStoreUtil.firstAliasOrThrow(keyStore);
+        assertEquals(ALIAS, alias);
     }
 
     @Test
-    public void getFirstCertAlias_PresentAlias() throws Exception {
-        KeyStore keystore = getAliasKeystoreByAlias("key1", router);
-        Optional<String> firstAlias = KeyStoreUtil.getFirstCertAlias(keystore);
-        assertTrue(firstAlias.isPresent());
-        assertEquals("key1", firstAlias.get());
+    void testFirstAliasOrThrowNotFound() throws KeyStoreException, IOException, CertificateException, NoSuchAlgorithmException {
+        java.security.KeyStore emptyKeyStore = java.security.KeyStore.getInstance("PKCS12");
+        emptyKeyStore.load(null, null);
+        assertThrows(RuntimeException.class, () -> KeyStoreUtil.firstAliasOrThrow(emptyKeyStore));
     }
 
     @Test
-    public void getFirstCertAlias_MultipleAliases() throws Exception {
-        KeyStore keystore = getAliasKeystoreByAlias("key2", router);
-        Optional<String> firstAlias = KeyStoreUtil.getFirstCertAlias(keystore);
-        assertTrue(firstAlias.isPresent());
-        assertEquals("key1", firstAlias.get());
+    void testAliasOrThrowFound() throws KeyStoreException {
+        String alias = KeyStoreUtil.aliasOrThrow(keyStore, ALIAS);
+        assertEquals(ALIAS, alias);
     }
 
     @Test
-    public void getFirstCertAlias_EmptyKeystore() throws Exception {
-        KeyStore keystore = getAliasKeystoreByAlias("key3", router);
-        Optional<String> firstAlias = KeyStoreUtil.getFirstCertAlias(keystore);
-        assertEquals("",firstAlias.get());
+    void testAliasOrThrowNotFound() {
+        assertThrows(RuntimeException.class, () -> KeyStoreUtil.aliasOrThrow(keyStore, "nonexistent"));
     }
 
-    private static @NotNull KeyStore getAliasKeystoreByAlias(String alias, Router router) throws Exception {
-        return openKeyStore(new com.predic8.membrane.core.config.security.KeyStore() {{
-            setLocation("classpath:/alias-keystore.p12");
-            setKeyPassword("secret");
-            setKeyAlias(alias);
-        }}, "PKCS12", "secret".toCharArray(), router.getResolverMap(), router.getBaseLocation());
+    @Test
+    void testGetFirstCertAliasFound() throws KeyStoreException {
+        Optional<String> alias = KeyStoreUtil.getFirstCertAlias(keyStore);
+        assertTrue(alias.isPresent());
+        assertEquals(ALIAS, alias.get());
+    }
+
+    @Test
+    void testGetFirstCertAliasNotFound() throws KeyStoreException, IOException, CertificateException, NoSuchAlgorithmException {
+        java.security.KeyStore emptyKeyStore = java.security.KeyStore.getInstance("PKCS12");
+        emptyKeyStore.load(null, null);
+        Optional<String> alias = KeyStoreUtil.getFirstCertAlias(emptyKeyStore);
+        assertFalse(alias.isPresent());
     }
 }
