5e69eb1445659f40f343c6c0ac18ad8d05ffcd64
Christian Gördes <goerdes@predic8.de>
2024-09-04 14:49:40 +0200

    Implement Shadowing Interceptor for concurrent request handling
    - Move `ShadowingInterceptor` to a new package `shadowing`
    - Refactor request handling to log status codes from shadow targets
    - Update example configurations to demonstrate shadow requests
    - Enhance README with instructions and configurations for running the example

---
+++ core/src/main/java/com/predic8/membrane/core/interceptor/shadowing/ShadowingInterceptor.java
@@ -0,0 +1,110 @@
+package com.predic8.membrane.core.interceptor.shadowing;
+
+import com.predic8.membrane.annot.MCChildElement;
+import com.predic8.membrane.annot.MCElement;
+import com.predic8.membrane.core.exchange.Exchange;
+import com.predic8.membrane.core.http.AbstractBody;
+import com.predic8.membrane.core.http.Chunk;
+import com.predic8.membrane.core.http.MessageObserver;
+import com.predic8.membrane.core.http.Request;
+import com.predic8.membrane.core.interceptor.AbstractInterceptor;
+import com.predic8.membrane.core.interceptor.Outcome;
+import com.predic8.membrane.core.rules.AbstractServiceProxy.Target;
+import com.predic8.membrane.core.transport.http.HttpClient;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.ExecutorService;
+
+import static com.predic8.membrane.core.interceptor.Outcome.CONTINUE;
+import static java.util.concurrent.Executors.newCachedThreadPool;
+
+@MCElement(name="shadowing")
+public class ShadowingInterceptor extends AbstractInterceptor {
+
+    private static final HttpClient client = new HttpClient();
+    private static final Logger log = LoggerFactory.getLogger(ShadowingInterceptor.class);
+
+    private List<Target> targets = new ArrayList<>();
+
+    @Override
+    public Outcome handleRequest(Exchange exc) throws Exception {
+        exc.getRequest().getBody().getObservers().add(new MessageObserver() {
+            @Override
+            public void bodyRequested(AbstractBody body) {}
+            @Override
+            public void bodyChunk(Chunk chunk) {}
+            @Override
+            public void bodyChunk(byte[] buffer, int offset, int length) {}
+
+            @Override
+            public void bodyComplete(AbstractBody body) {
+                if(exc.getProperty("shadow-request-send") == null) {
+                    cloneRequestAndSend(body);
+                    exc.setProperty("shadow-request-send", true);
+                }
+            }
+        });
+        return CONTINUE;
+    }
+
+    public void cloneRequestAndSend(AbstractBody body) {
+        try (ExecutorService executor = newCachedThreadPool()) {
+            for (Target target : targets) {
+                Exchange exc;
+                try {
+                    exc = new Request.Builder()
+                            .body(body.getContent())
+                            .get(getDestFromTarget(target, router.getParentProxy(this).getKey().getPath()))
+                            .buildExchange();
+                } catch (Exception e) {
+                    log.error("Error creating request for target {}", target, e);
+                    continue;
+                }
+
+                executor.submit(() -> {
+                    try {
+                        Exchange res = performCall(exc);
+                        if (res.getResponse().getStatusCode() >= 500)
+                            log.info("{} returned StatusCode {}", res.getDestinations().get(0), res.getResponse().getStatusCode());
+                    } catch (Exception e) {
+                        log.error("Error performing call for target {}", target, e);
+                    }
+                });
+            }
+        }
+    }
+
+
+    private static String getDestFromTarget(Target t, String path) {
+        return (t.getUrl() != null) ? t.getUrl() : extracted(t, path);
+    }
+
+    @SuppressWarnings("HttpUrlsUsage")
+    private static String extracted(Target t, String path) {
+        return ((t.getSslParser() != null) ? "https://" : "http://") +
+                t.getHost() +
+                ":" +
+                t.getPort() +
+                ((path != null) ? path : "");
+    }
+
+    private static Exchange performCall(Exchange exchange) {
+        try {
+            return client.call(exchange);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @MCChildElement
+    public void setTargets(List<Target> targets) {
+        this.targets = targets;
+    }
+
+    public List<Target> getTargets() {
+        return targets;
+    }
+}
