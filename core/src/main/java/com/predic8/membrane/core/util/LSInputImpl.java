f1fa151a9371ad96c6b327aaaeb25ada4b1560b1
Thomas Bayer <bayer@predic8.de>
2024-11-14 09:57:53 +0100

    Cleanup

--- core/src/main/java/com/predic8/membrane/core/util/LSInputImpl.java
+++ core/src/main/java/com/predic8/membrane/core/util/LSInputImpl.java
@@ -38,42 +38,34 @@ public class LSInputImpl implements LSInput {
 				.get();
 	}
 
-	@Override
 	public String getPublicId() {
 		return publicId;
 	}
 
-	@Override
 	public void setPublicId(String publicId) {
 		this.publicId = publicId;
 	}
 
-	@Override
 	public String getBaseURI() {
 		return null;
 	}
 
-	@Override
 	public InputStream getByteStream() {
 		return null;
 	}
 
-	@Override
 	public boolean getCertifiedText() {
 		return false;
 	}
 
-	@Override
 	public Reader getCharacterStream() {
 		return null;
 	}
 
-	@Override
 	public String getEncoding() {
 		return UTF_8.name();
 	}
 
-	@Override
 	public String getStringData() {
 		synchronized (inputStream) {
 			try {
@@ -89,42 +81,34 @@ public class LSInputImpl implements LSInput {
 		return new String(inputStream.readAllBytes(), UTF_8);
 	}
 
-	@Override
 	public void setBaseURI(String baseURI) {
 		//ignore
 	}
 
-	@Override
 	public void setByteStream(InputStream byteStream) {
 		this.inputStream = byteStream;
 	}
 
-	@Override
 	public void setCertifiedText(boolean certifiedText) {
 		//ignore
 	}
 
-	@Override
 	public void setCharacterStream(Reader characterStream) {
 		//ignore
 	}
 
-	@Override
 	public void setEncoding(String encoding) {
 		//ignore
 	}
 
-	@Override
 	public void setStringData(String stringData) {
 		//ignore
 	}
 
-	@Override
 	public String getSystemId() {
 		return systemId;
 	}
 
-	@Override
 	public void setSystemId(String systemId) {
 		this.systemId = systemId;
 	}
