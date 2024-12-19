3b9257ce03c139ae44e39fe3053930786cb746f8
Thomas Bayer <bayer@predic8.de>
2024-12-19 08:50:09 +0100

    Formatted code

--- core/src/test/java/com/predic8/membrane/core/interceptor/xmlprotection/XMLProtectionInterceptorTest.java
+++ core/src/test/java/com/predic8/membrane/core/interceptor/xmlprotection/XMLProtectionInterceptorTest.java
@@ -25,52 +25,52 @@ import static com.predic8.membrane.core.interceptor.Outcome.*;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class XMLProtectionInterceptorTest {
-	private static Exchange exc;
-	private static XMLProtectionInterceptor interceptor;
+    private static Exchange exc;
+    private static XMLProtectionInterceptor interceptor;
 
-	@BeforeAll
-	public static void setUp() throws Exception {
-		exc = new Exchange(null);
-		exc.setRequest(MessageUtil.getGetRequest("/axis2/services/BLZService"));
-		exc.setOriginalHostHeader("thomas-bayer.com:80");
+    @BeforeAll
+    public static void setUp() throws Exception {
+        exc = new Exchange(null);
+        exc.setRequest(MessageUtil.getGetRequest("/axis2/services/BLZService"));
+        exc.setOriginalHostHeader("thomas-bayer.com:80");
 
-		interceptor = new XMLProtectionInterceptor();
-	}
+        interceptor = new XMLProtectionInterceptor();
+    }
 
-	private void runOn(String resource, boolean expectSuccess) throws Exception {
-		exc.getRequest().getHeader().setContentType(APPLICATION_XML);
-		exc.getRequest().setBodyContent(ByteUtil.getByteArrayData(this.getClass().getResourceAsStream(resource)));
-		Outcome outcome = interceptor.handleRequest(exc);
-		assertEquals(expectSuccess ? CONTINUE : ABORT, outcome);
-	}
+    private void runOn(String resource, boolean expectSuccess) throws Exception {
+        exc.getRequest().getHeader().setContentType(APPLICATION_XML);
+        exc.getRequest().setBodyContent(ByteUtil.getByteArrayData(this.getClass().getResourceAsStream(resource)));
+        Outcome outcome = interceptor.handleRequest(exc);
+        assertEquals(expectSuccess ? CONTINUE : ABORT, outcome);
+    }
 
-	@Test
-	void testInvariant() throws Exception {
-		runOn("/customer.xml", true);
-	}
+    @Test
+    void testInvariant() throws Exception {
+        runOn("/customer.xml", true);
+    }
 
-	@Test
-	void testNotWellformed() throws Exception {
-		runOn("/xml/not-wellformed.xml", false);
-	}
+    @Test
+    void testNotWellformed() throws Exception {
+        runOn("/xml/not-wellformed.xml", false);
+    }
 
-	@Test
-	void removeDTD() throws Exception {
-		exc.setRequest(Request.post("/").body("""
-			<?xml  version="1.0" encoding="ISO-8859-1"?>
-			<!DOCTYPE foo [
-			     <!ELEMENT foo ANY >
-			   ]>
-			<foo/>
-			""").contentType(APPLICATION_XML).build());
+    @Test
+    void removeDTD() throws Exception {
+        exc.setRequest(Request.post("/").body("""
+                <?xml  version="1.0" encoding="ISO-8859-1"?>
+                <!DOCTYPE foo [
+                     <!ELEMENT foo ANY >
+                   ]>
+                <foo/>
+                """).contentType(APPLICATION_XML).build());
 
-		// Should pass
+        // Should pass
         assertEquals(CONTINUE, interceptor.handleRequest(exc));
 
-		// Should still contain the XML
-		assertTrue(exc.getRequest().getBodyAsStringDecoded().contains("<foo"));
+        // Should still contain the XML
+        assertTrue(exc.getRequest().getBodyAsStringDecoded().contains("<foo"));
 
-		// DTD should be removed
+        // DTD should be removed
         assertFalse(exc.getRequest().getBodyAsStringDecoded().contains("DOCTYPE"));
-	}
+    }
 }
