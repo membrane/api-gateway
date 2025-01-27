2866d2b4e660b1763aae91f1240bc1c9ae5d56e8
Tobias Polley <polley@predic8.de>
2025-01-27 18:25:18 +0100

    added brotli test, switched brotli decoder by a working copy (#1532)
    Co-authored-by: Thomas Bayer <bayer@predic8.de>

---
+++ core/src/main/java/org/brotli/dec/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Copied from https://github.com/google/brotli/tree/master/java/org/brotli/dec under the MIT license.
+ * See file LICENSE for detail or copy at https://opensource.org/licenses/MIT
+ */
