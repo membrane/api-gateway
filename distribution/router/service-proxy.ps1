1636156f97d19264a54e733120f0db65a792ddb5
Torben Burchgart <burchgart@predic8.de>
2025-02-05 16:28:00 +0100

    Made start scripts accept cli params again

--- distribution/router/service-proxy.ps1
+++ distribution/router/service-proxy.ps1
@@ -1,12 +1,15 @@
 $required_version = "21"
 
 function Start-MembraneService {
-    param([string]$membrane_home)
+    param(
+        [string]$membrane_home,
+        [string[]]$ExtraArgs
+    )
 
     $env:MEMBRANE_HOME = $membrane_home
     $CLASSPATH = "$membrane_home\conf;$membrane_home\lib\*"
     Write-Host "Starting: $membrane_home CL: $CLASSPATH"
-    & java -cp "$CLASSPATH" com.predic8.membrane.core.cli.RouterCLI
+    & java -cp "$CLASSPATH" com.predic8.membrane.core.cli.RouterCLI @ExtraArgs
 }
 
 function Find-MembraneDirectory {
@@ -22,9 +25,12 @@ function Find-MembraneDirectory {
 }
 
 function Start-Membrane {
+    param(
+       [string[]]$ExtraArgs
+    )
     $membrane_home = Find-MembraneDirectory (Get-Location).Path
     if ($membrane_home) {
-        Start-MembraneService $membrane_home
+        Start-MembraneService $membrane_home @ExtraArgs
     } else {
         Write-Host "Could not start Membrane. Ensure the directory structure is correct."
     }
@@ -43,7 +49,7 @@ try {
 
     if (-not $version_line) {
         Write-Host "WARNING: Could not determine Java version. Make sure Java version is at least $required_version. Proceeding anyway..."
-        Start-Membrane
+        Start-Membrane @args
         exit 0
     }
 
@@ -52,7 +58,7 @@ try {
 
     if ($current_version -match '^\d+$') {
         if ([int]$current_version -ge [int]$required_version) {
-            Start-Membrane
+            Start-Membrane @args
             exit 0
         } else {
             Write-Host "Java version mismatch: Required=$required_version, Installed=$full_version"
@@ -60,7 +66,7 @@ try {
         }
     } else {
         Write-Host "WARNING: Could not parse Java version. Make sure your Java version is at least $required_version. Proceeding anyway..."
-        Start-Membrane
+        Start-Membrane @args
         exit 0
     }
 } catch {
