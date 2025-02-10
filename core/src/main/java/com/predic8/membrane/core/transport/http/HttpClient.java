a0650a9138fd1cfb5f30bdf95785c91da1c12323
Luca <lucamadio@hotmail.it>
2025-02-10 14:20:13 +0100

    Update HttpClient.java

--- core/src/main/java/com/predic8/membrane/core/transport/http/HttpClient.java
+++ core/src/main/java/com/predic8/membrane/core/transport/http/HttpClient.java
@@ -1,4 +1,4 @@
-    private static final String[] HTTP1_PROTOCOLS = new String[]{"http/1.1"};/* Copyright 2009, 2012 predic8 GmbH, www.predic8.com
+   /* Copyright 2009, 2012 predic8 GmbH, www.predic8.com
 
    Licensed under the Apache License, Version 2.0 (the "License");
    you may not use this file except in compliance with the License.
