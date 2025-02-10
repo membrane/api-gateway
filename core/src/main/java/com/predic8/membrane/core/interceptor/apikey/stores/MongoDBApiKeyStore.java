efc212964983eefec1e5854297fffb27be4bfa53
Russel Marcelo <russelmarcelo1509@gmail.com>
2025-02-10 16:29:10 +0100

    wip: mongodb apikey store

--- core/src/main/java/com/predic8/membrane/core/interceptor/apikey/stores/MongoDBApiKeyStore.java
+++ core/src/main/java/com/predic8/membrane/core/interceptor/apikey/stores/MongoDBApiKeyStore.java
@@ -9,9 +9,9 @@ import com.predic8.membrane.core.Router;
 import com.predic8.membrane.core.util.ConfigurationException;
 import org.bson.Document;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
+import java.util.UUID;
 
 @MCElement(name = "mongoDBApiKeyStore", topLevel = false)
 public class MongoDBApiKeyStore implements ApiKeyStore {
@@ -19,7 +19,6 @@ public class MongoDBApiKeyStore implements ApiKeyStore {
     private String connectionString;
     private String databaseName;
     private KeyCollection keyCollection;
-    private ScopeCollection scopeCollection;
 
     private MongoClient mongoClient;
     private MongoDatabase database;
@@ -29,7 +28,6 @@ public class MongoDBApiKeyStore implements ApiKeyStore {
         try {
             mongoClient = MongoClients.create(connectionString);
             database = mongoClient.getDatabase(databaseName);
-            createCollectionsIfNotExist();
         } catch (Exception e) {
             throw new ConfigurationException("Failed to connect to MongoDB", e);
         }
@@ -37,40 +35,14 @@ public class MongoDBApiKeyStore implements ApiKeyStore {
 
     @Override
     public Optional<List<String>> getScopes(String apiKey) throws UnauthorizedApiKeyException {
-        checkApiKey(apiKey);
-        return fetchScopes(apiKey);
-    }
+        Document apiKeyDoc = database.getCollection(keyCollection.getName())
+                .find(Filters.eq("_id", apiKey)).first();
 
-    private void checkApiKey(String apiKey) throws UnauthorizedApiKeyException {
-        if (database.getCollection(keyCollection.getName()).find(Filters.eq("apikey", apiKey)).first() == null) {
+        if (apiKeyDoc == null) {
             throw new UnauthorizedApiKeyException();
         }
-    }
 
-    private Optional<List<String>> fetchScopes(String apiKey) {
-        List<String> scopes = new ArrayList<>();
-        for (Document doc : database.getCollection(scopeCollection.getName()).find(Filters.eq("apikey", apiKey))) {
-            scopes.add(doc.getString("scope"));
-        }
-        return Optional.of(scopes);
-    }
-
-    private void createCollectionsIfNotExist() {
-        if (!collectionExists(keyCollection.getName())) {
-            database.createCollection(keyCollection.getName());
-        }
-        if (!collectionExists(scopeCollection.getName())) {
-            database.createCollection(scopeCollection.getName());
-        }
-    }
-
-    private boolean collectionExists(String collectionName) {
-        for (String name : database.listCollectionNames()) {
-            if (name.equalsIgnoreCase(collectionName)) {
-                return true;
-            }
-        }
-        return false;
+        return Optional.ofNullable(apiKeyDoc.getList("scopes", String.class));
     }
 
     @MCAttribute()
@@ -88,11 +60,6 @@ public class MongoDBApiKeyStore implements ApiKeyStore {
         this.keyCollection = keyCollection;
     }
 
-    @MCChildElement(order = 1)
-    public void setScopeCollection(ScopeCollection scopeCollection) {
-        this.scopeCollection = scopeCollection;
-    }
-
     public String getConnectionString() {
         return connectionString;
     }
@@ -103,9 +70,5 @@ public class MongoDBApiKeyStore implements ApiKeyStore {
 
     public String getKeyCollection() {
         return keyCollection.getName();
-    }
-
-    public String getScopeCollection() {
-        return scopeCollection.getName();
     }
 }
--- core/src/main/java/com/predic8/membrane/core/interceptor/apikey/stores/ScopeCollection.java
+++
@@ -1,7 +0,0 @@
-package com.predic8.membrane.core.interceptor.apikey.stores;
-
-import com.predic8.membrane.annot.MCElement;
-
-@MCElement(name = "scopeCollection", mixed = true)
-public class ScopeCollection extends CollectionName{
-}
