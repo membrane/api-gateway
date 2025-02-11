1d6913e4cc28fa052170d29a2041e019e1d2d72b
Luca <lucamadio@hotmail.it>
2025-02-11 14:53:50 +0100

    Reusable connection in http 1.1 using hashMap availableConnections of ConnectionManager (#1595)
    * Update ConnectionKey.java
    
    Use Arrays class instead of Objects in equals and hashCode methods for applicationProtocols field
    
    * Update HttpClient.java
    
    added HTTP1_PROTOCOL  and use it in getApplicationProtocol method
    
    * Update ConnectionKey.java
    
    add import Arrays
    
    * Update HttpClient.java
    
    * Update ConnectionKey.java
    
    fix hashCode implementation
    
    ---------
    
    Co-authored-by: Tobias Polley <polley@predic8.de>

--- core/src/main/java/com/predic8/membrane/core/transport/http/ConnectionKey.java
+++ core/src/main/java/com/predic8/membrane/core/transport/http/ConnectionKey.java
@@ -16,7 +16,7 @@ package com.predic8.membrane.core.transport.http;
 import com.google.common.base.Objects;
 import com.predic8.membrane.core.transport.http.client.ProxyConfiguration;
 import com.predic8.membrane.core.transport.ssl.SSLProvider;
-
+import java.util.Arrays;
 import javax.annotation.Nullable;
 
 public class ConnectionKey {
@@ -48,7 +48,7 @@ public class ConnectionKey {
 
     @Override
     public int hashCode() {
-        return Objects.hashCode(host, port, sslProvider, serverName, proxy, proxySSLProvider, applicationProtocols);
+        return Objects.hashCode(host, port, sslProvider, serverName, proxy, proxySSLProvider, Arrays.hashCode(applicationProtocols));
     }
 
     @Override
@@ -62,7 +62,7 @@ public class ConnectionKey {
                 && Objects.equal(serverName, other.serverName)
                 && Objects.equal(proxy, other.proxy)
                 && Objects.equal(proxySSLProvider, other.proxySSLProvider)
-                && Objects.equal(applicationProtocols, other.applicationProtocols);
+                && Arrays.equals(applicationProtocols, other.applicationProtocols);
     }
 
     @Override
