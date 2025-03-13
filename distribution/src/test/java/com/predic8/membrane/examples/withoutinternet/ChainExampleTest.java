48fcd7e628332140de8718500f60f3f8423869cf
Christian Gördes <christian.goerdes@outlook.de>
2025-03-13 12:25:09 +0100

    move

--- distribution/src/test/java/com/predic8/membrane/examples/withoutinternet/ChainExampleTest.java
+++ distribution/src/test/java/com/predic8/membrane/examples/withoutinternet/ChainExampleTest.java
@@ -1,4 +1,4 @@
-package com.predic8.membrane.examples.tests;
+package com.predic8.membrane.examples.withoutinternet;
 
 import com.predic8.membrane.examples.util.AbstractSampleMembraneStartStopTestcase;
 import org.junit.jupiter.api.Test;
@@ -6,7 +6,6 @@ import org.junit.jupiter.api.Test;
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.Matchers.containsString;
 
-// TODO move to `withinternet` directory when pr #1631 is merged
 public class ChainExampleTest extends AbstractSampleMembraneStartStopTestcase {
 
     @Override
