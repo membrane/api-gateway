eabc76d633f07876e69ff035b4112322dc134787
t-burch <119930761+t-burch@users.noreply.github.com>
2025-03-13 12:08:28 +0100

    Example Test Restructuring  (#1631)
    * restructuring (wip)
    
    * refactoring and fixes
    
    * excluded test
    
    * rm
    
    * .
    
    * add license
    
    * moved
    
    ---------
    
    Co-authored-by: Christian Gördes <goerdes@predic8.de>
    Co-authored-by: Christian Gördes <christian.goerdes@outlook.de>
    Co-authored-by: Christian Gördes <118011644+christiangoerdes@users.noreply.github.com>

---
+++ distribution/src/test/java/com/predic8/membrane/examples/withinternet/test/LoggingExampleTest.java
@@ -0,0 +1,40 @@
+/* Copyright 2012 predic8 GmbH, www.predic8.com
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+   http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License. */
+
+package com.predic8.membrane.examples.withinternet.test;
+
+import com.predic8.membrane.examples.util.DistributionExtractingTestcase;
+import com.predic8.membrane.examples.util.Process2;
+import com.predic8.membrane.examples.util.SubstringWaitableConsoleEvent;
+import com.predic8.membrane.test.HttpAssertions;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class LoggingExampleTest extends DistributionExtractingTestcase {
+
+	@Override
+	protected String getExampleDirName() {
+		return "logging/console";
+	}
+
+	@Test
+	public void test() throws Exception {
+		try(Process2 sl = startServiceProxyScript(); HttpAssertions ha = new HttpAssertions()) {
+			SubstringWaitableConsoleEvent logged = new SubstringWaitableConsoleEvent(sl, "HTTP/1.1");
+			ha.getAndAssert200("http://localhost:2000/");
+			assertTrue(logged.occurred());
+		}
+	}
+}
