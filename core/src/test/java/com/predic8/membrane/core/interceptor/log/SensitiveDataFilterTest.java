950ef89d006a6be92d86e420b9c0f64cb515ce4c
Russel Marcelo <russelmarcelo1509@gmail.com>
2025-09-11 14:46:53 +0200

    add more tests

--- core/src/test/java/com/predic8/membrane/core/interceptor/log/SensitiveDataFilterTest.java
+++ core/src/test/java/com/predic8/membrane/core/interceptor/log/SensitiveDataFilterTest.java
@@ -1,9 +1,7 @@
 package com.predic8.membrane.core.interceptor.log;
 
 import com.predic8.membrane.core.http.Header;
-import com.predic8.membrane.core.http.Message;
 import com.predic8.membrane.core.http.Request;
-import com.predic8.membrane.core.util.URIFactory;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -44,6 +42,20 @@ class SensitiveDataFilterTest {
     @Test
     void maskStartLine() throws URISyntaxException {
         assertEquals("GET /v1/users?token=********&x=1&id_token=******** HTTP/1.1\r\n",
-                filter.maskStartLine(new Request.Builder().get("/v1/users?token=abc123&x=1&id_token=XYZ").body("").build()));
+                filter.maskStartLine(Request.get("/v1/users?token=abc123&x=1&id_token=XYZ").body("").build()));
+
+        assertEquals("GET /v1/res?api-key=********&api_key=********&x-api-key=******** HTTP/1.1\r\n",
+                filter.maskStartLine(Request.get("/v1/res?api-key=A&api_key=B&x-api-key=C").build()));
+
+        assertEquals("GET /q?password=********&keep=ok HTTP/1.1\r\n",
+                filter.maskStartLine(Request.get("/q?password=&keep=ok").build()));
+
+        assertEquals("GET /v1/oauth?client_secret=********&keep=ok HTTP/1.1\r\n",
+                filter.maskStartLine(Request.get("/v1/oauth?client_secret=mySecret123&keep=ok").build())
+        );
+
+        assertEquals("GET /v1/auth?access_token=********&keep=ok HTTP/1.1\r\n",
+                filter.maskStartLine(Request.get("/v1/auth?access_token=superSecret456&keep=ok").build())
+        );
     }
 }
