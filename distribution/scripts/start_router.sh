c0a1940d98cbb8306e6f2192305c41ff73cb7470
Christian Gördes <christian.goerdes@outlook.de>
2025-11-13 16:00:02 +0100

    add conf prio

--- distribution/scripts/start_router.sh
+++ distribution/scripts/start_router.sh
@@ -4,12 +4,41 @@
 CLASSPATH="$MEMBRANE_HOME/conf:$MEMBRANE_HOME/lib/*"
 
 if [ $# -eq 0 ]; then
-  if [ -n "${MEMBRANE_CALLER_DIR:-}" ] && [ -f "$MEMBRANE_CALLER_DIR/proxies.xml" ]; then
+  CONFIG_FOUND=""
+
+  # Prio 1: apis.yaml|apis.yml in caller dir
+  if [ -n "${MEMBRANE_CALLER_DIR:-}" ] && [ -f "$MEMBRANE_CALLER_DIR/apis.yaml" ]; then
+    set -- -c "$MEMBRANE_CALLER_DIR/apis.yaml" "$@"
+    CONFIG_FOUND=1
+  elif [ -n "${MEMBRANE_CALLER_DIR:-}" ] && [ -f "$MEMBRANE_CALLER_DIR/apis.yml" ]; then
+    set -- -c "$MEMBRANE_CALLER_DIR/apis.yml" "$@"
+    CONFIG_FOUND=1
+
+  # Prio 2: proxies.xml in caller dir
+  elif [ -n "${MEMBRANE_CALLER_DIR:-}" ] && [ -f "$MEMBRANE_CALLER_DIR/proxies.xml" ]; then
     set -- -c "$MEMBRANE_CALLER_DIR/proxies.xml" "$@"
+    CONFIG_FOUND=1
+
+  # Prio 3: apis.yaml|yml in MEMBRANE_HOME
+  elif [ -f "$MEMBRANE_HOME/conf/apis.yaml" ]; then
+    set -- -c "$MEMBRANE_HOME/conf/apis.yaml" "$@"
+    CONFIG_FOUND=1
+  elif [ -f "$MEMBRANE_HOME/conf/apis.yml" ]; then
+    set -- -c "$MEMBRANE_HOME/conf/apis.yml" "$@"
+    CONFIG_FOUND=1
+
+  # Prio 4: proxies.xml in MEMBRANE_HOME
   elif [ -f "$MEMBRANE_HOME/conf/proxies.xml" ]; then
     set -- -c "$MEMBRANE_HOME/conf/proxies.xml" "$@"
+    CONFIG_FOUND=1
+  fi
+
+  if [ -z "$CONFIG_FOUND" ]; then
+    echo "No configuration file found (apis.yaml, apis.yml or proxies.xml). Provide one of these or use -c <file>." >&2
+    exit 1
   fi
 fi
+
 
 normalize_java_opts() {
   [ -n "${JAVA_OPTS:-}" ] || return 0
