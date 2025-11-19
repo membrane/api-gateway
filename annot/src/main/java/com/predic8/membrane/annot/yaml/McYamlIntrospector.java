192676bc7101a2047880f27b757ae711cf68ab9c
Christian Gördes <christian.goerdes@outlook.de>
2025-11-19 14:55:25 +0100

    fix imports and package

--- annot/src/main/java/com/predic8/membrane/annot/yaml/McYamlIntrospector.java
+++ annot/src/main/java/com/predic8/membrane/annot/yaml/McYamlIntrospector.java
@@ -12,7 +12,9 @@
    See the License for the specific language governing permissions and
    limitations under the License. */
 
-package com.predic8.membrane.annot;
+package com.predic8.membrane.annot.yaml;
+
+import com.predic8.membrane.annot.*;
 
 import java.lang.reflect.Method;
 import java.util.Arrays;
