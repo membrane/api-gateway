e097e7c844e8540a9b6d68a2c1ae420f45a4a512
Christian Gördes <christian.goerdes@outlook.de>
2025-12-17 09:25:15 +0100

    Refactor `ComponentClassGenerator` and `BeanClassGenerator` to extend abstract `ClassGenerator`
    - Introduced `ClassGenerator` as an abstract base class to encapsulate shared functionality.
    - Simplified `ComponentClassGenerator` and `BeanClassGenerator` by moving common logic to `ClassGenerator`.

--- annot/src/main/java/com/predic8/membrane/annot/generator/BeanClassGenerator.java
+++ annot/src/main/java/com/predic8/membrane/annot/generator/BeanClassGenerator.java
@@ -1,193 +1,150 @@
 package com.predic8.membrane.annot.generator;
 
-import com.predic8.membrane.annot.model.MainInfo;
-import com.predic8.membrane.annot.model.Model;
-
-import javax.annotation.processing.FilerException;
 import javax.annotation.processing.ProcessingEnvironment;
-import javax.tools.FileObject;
-import java.io.BufferedWriter;
-import java.io.IOException;
 
-public class BeanClassGenerator {
-    private final ProcessingEnvironment processingEnv;
+public class BeanClassGenerator extends ClassGenerator{
 
     public BeanClassGenerator(ProcessingEnvironment processingEnv) {
-        this.processingEnv = processingEnv;
+        super(processingEnv);
     }
 
-    /**
-     * @return true if the file was written, false if it already existed
-     */
-    public boolean writeJava(Model m) throws IOException {
-        for (MainInfo main : m.getMains()) {
-            try {
-                FileObject o = processingEnv.getFiler().createSourceFile(
-                        main.getAnnotation().outputPackage() + ".Bean"
-                );
+    @Override
+    protected String getClassName() {
+        return "Bean";
+    }
 
-                try (BufferedWriter bw = new BufferedWriter(o.openWriter())) {
-                    String copyright = """
-                            /* Copyright 2025 predic8 GmbH, www.predic8.com
-                            
-                               Licensed under the Apache License, Version 2.0 (the "License");
-                               you may not use this file except in compliance with the License.
-                               You may obtain a copy of the License at
-                            
-                               http://www.apache.org/licenses/LICENSE-2.0
-                            
-                               Unless required by applicable law or agreed to in writing, software
-                               distributed under the License is distributed on an "AS IS" BASIS,
-                               WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-                               See the License for the specific language governing permissions and
-                               limitations under the License. */
-                            """;
-
-                    bw.write(copyright +
-                            "\r\n" +
-                            "package " + main.getAnnotation().outputPackage() + ";\r\n" +
-                            """
-                                    import com.predic8.membrane.annot.MCAttribute;
-                                    import com.predic8.membrane.annot.MCChildElement;
-                                    import com.predic8.membrane.annot.MCElement;
-                                    
-                                    import java.util.ArrayList;
-                                    import java.util.List;
-                                    
-                                    /**
-                                     * Spring-like bean definition usable in YAML components:
-                                     * components:
-                                     *   <id>:
-                                     *     bean:
-                                     *       class: com.example.MyInterceptor
-                                     *       scope: SINGLETON
-                                     *       constructorArgs:
-                                     *         - constructorArg:
-                                     *             value: foo
-                                     *       properties:
-                                     *         - property:
-                                     *             name: bar
-                                     *             value: baz
-                                     */
-                                    @MCElement(name = "bean")
-                                    public class Bean {
-                                    
-                                        private String className;
-                                        private Scope scope = Scope.SINGLETON;
-                                        private List<ConstructorArg> constructorArgs = new ArrayList<>();
-                                        private List<Property> properties = new ArrayList<>();
-                                    
-                                        @MCAttribute
-                                        public void setClass(String className) {
-                                            this.className = className;
-                                        }
-                                    
-                                        public String getClassName() {
-                                            return className;
-                                        }
-                                    
-                                        @MCAttribute
-                                        public void setScope(Scope scope) {
-                                            this.scope = scope;
-                                        }
-                                    
-                                        public Scope getScope() {
-                                            return scope;
-                                        }
-                                    
-                                        @MCChildElement(order = 3)
-                                        public void setConstructorArgs(List<ConstructorArg> constructorArgs) {
-                                            this.constructorArgs = constructorArgs;
-                                        }
-                                    
-                                        public List<ConstructorArg> getConstructorArgs() {
-                                            return constructorArgs;
-                                        }
-                                    
-                                        @MCChildElement
-                                        public void setProperties(List<Property> properties) {
-                                            this.properties = properties;
-                                        }
-                                    
-                                        public List<Property> getProperties() {
-                                            return properties;
-                                        }
-                                    
-                                        public enum Scope {
-                                            SINGLETON,
-                                            PROTOTYPE
-                                        }
-                                    
-                                        @MCElement(name = "constructorArg", component = false)
-                                        public static class ConstructorArg {
-                                            private String value;
-                                            private String ref;
-                                    
-                                            @MCAttribute
-                                            public void setValue(String value) {
-                                                this.value = value;
-                                            }
-                                    
-                                            public String getValue() {
-                                                return value;
-                                            }
-                                    
-                                            @MCAttribute
-                                            public void setRef(String ref) {
-                                                this.ref = ref;
-                                            }
-                                    
-                                            public String getRef() {
-                                                return ref;
-                                            }
-                                        }
-                                    
-                                        @MCElement(name = "property", component = false)
-                                        public static class Property {
-                                            private String name;
-                                            private String value;
-                                            private String ref;
-                                    
-                                            @MCAttribute
-                                            public void setName(String name) {
-                                                this.name = name;
-                                            }
-                                    
-                                            public String getName() {
-                                                return name;
-                                            }
-                                    
-                                            @MCAttribute
-                                            public void setValue(String value) {
-                                                this.value = value;
-                                            }
-                                    
-                                            public String getValue() {
-                                                return value;
-                                            }
-                                    
-                                            @MCAttribute
-                                            public void setRef(String ref) {
-                                                this.ref = ref;
-                                            }
-                                    
-                                            public String getRef() {
-                                                return ref;
-                                            }
-                                        }
-                                    }
-                                    """);
+    @Override
+    protected String getClassImpl() {
+        return """
+                import com.predic8.membrane.annot.MCAttribute;
+                import com.predic8.membrane.annot.MCChildElement;
+                import com.predic8.membrane.annot.MCElement;
+                
+                import java.util.ArrayList;
+                import java.util.List;
+                
+                /**
+                 * Spring-like bean definition usable in YAML components:
+                 * components:
+                 *   <id>:
+                 *     bean:
+                 *       class: com.example.MyInterceptor
+                 *       scope: SINGLETON
+                 *       constructorArgs:
+                 *         - constructorArg:
+                 *             value: foo
+                 *       properties:
+                 *         - property:
+                 *             name: bar
+                 *             value: baz
+                 */
+                @MCElement(name = "bean")
+                public class Bean {
+                
+                    private String className;
+                    private Scope scope = Scope.SINGLETON;
+                    private List<ConstructorArg> constructorArgs = new ArrayList<>();
+                    private List<Property> properties = new ArrayList<>();
+                
+                    @MCAttribute
+                    public void setClass(String className) {
+                        this.className = className;
+                    }
+                
+                    public String getClassName() {
+                        return className;
+                    }
+                
+                    @MCAttribute
+                    public void setScope(Scope scope) {
+                        this.scope = scope;
+                    }
+                
+                    public Scope getScope() {
+                        return scope;
+                    }
+                
+                    @MCChildElement(order = 3)
+                    public void setConstructorArgs(List<ConstructorArg> constructorArgs) {
+                        this.constructorArgs = constructorArgs;
+                    }
+                
+                    public List<ConstructorArg> getConstructorArgs() {
+                        return constructorArgs;
+                    }
+                
+                    @MCChildElement
+                    public void setProperties(List<Property> properties) {
+                        this.properties = properties;
+                    }
+                
+                    public List<Property> getProperties() {
+                        return properties;
+                    }
+                
+                    public enum Scope {
+                        SINGLETON,
+                        PROTOTYPE
+                    }
+                
+                    @MCElement(name = "constructorArg", component = false)
+                    public static class ConstructorArg {
+                        private String value;
+                        private String ref;
+                
+                        @MCAttribute
+                        public void setValue(String value) {
+                            this.value = value;
+                        }
+                
+                        public String getValue() {
+                            return value;
+                        }
+                
+                        @MCAttribute
+                        public void setRef(String ref) {
+                            this.ref = ref;
+                        }
+                
+                        public String getRef() {
+                            return ref;
+                        }
+                    }
+                
+                    @MCElement(name = "property", component = false)
+                    public static class Property {
+                        private String name;
+                        private String value;
+                        private String ref;
+                
+                        @MCAttribute
+                        public void setName(String name) {
+                            this.name = name;
+                        }
+                
+                        public String getName() {
+                            return name;
+                        }
+                
+                        @MCAttribute
+                        public void setValue(String value) {
+                            this.value = value;
+                        }
+                
+                        public String getValue() {
+                            return value;
+                        }
+                
+                        @MCAttribute
+                        public void setRef(String ref) {
+                            this.ref = ref;
+                        }
+                
+                        public String getRef() {
+                            return ref;
+                        }
+                    }
                 }
-                return true;
-            } catch (FilerException e) {
-                if (e.getMessage().contains("Source file already created"))
-                    return false;
-                if (e.getMessage().contains("Attempt to recreate a file for"))
-                    return false;
-                throw e;
-            }
-        }
-
-        return false;
+                """;
     }
 }
--- annot/src/main/java/com/predic8/membrane/annot/generator/ComponentClassGenerator.java
+++ annot/src/main/java/com/predic8/membrane/annot/generator/ComponentClassGenerator.java
@@ -1,84 +1,44 @@
 package com.predic8.membrane.annot.generator;
 
-import com.predic8.membrane.annot.model.MainInfo;
-import com.predic8.membrane.annot.model.Model;
-
-import javax.annotation.processing.FilerException;
 import javax.annotation.processing.ProcessingEnvironment;
-import javax.tools.FileObject;
-import java.io.BufferedWriter;
-import java.io.IOException;
 
-public class ComponentClassGenerator {
-    private final ProcessingEnvironment processingEnv;
+public class ComponentClassGenerator extends ClassGenerator{
+
 
     public ComponentClassGenerator(ProcessingEnvironment processingEnv) {
-        this.processingEnv = processingEnv;
+        super(processingEnv);
     }
 
-    /**
-     * @return true if the file was written, false if it already existed
-     */
-    public boolean writeJava(Model m) throws IOException {
-        for (MainInfo main : m.getMains()) {
-            try {
-                FileObject o = processingEnv.getFiler().createSourceFile(
-                        main.getAnnotation().outputPackage() + ".Components"
-                );
-                try (BufferedWriter bw = new BufferedWriter(o.openWriter())) {
-                    String copyright = """
-                            /* Copyright 2025 predic8 GmbH, www.predic8.com
-                            
-                               Licensed under the Apache License, Version 2.0 (the "License");
-                               you may not use this file except in compliance with the License.
-                               You may obtain a copy of the License at
-                            
-                               http://www.apache.org/licenses/LICENSE-2.0
-                            
-                               Unless required by applicable law or agreed to in writing, software
-                               distributed under the License is distributed on an "AS IS" BASIS,
-                               WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-                               See the License for the specific language governing permissions and
-                               limitations under the License. */
-                            """;
-                    bw.write(copyright +
-                            "\r\n" +
-                            "package " + main.getAnnotation().outputPackage() + ";\r\n" +
-                            """
-                                    import com.predic8.membrane.annot.MCElement;
-                                    import com.predic8.membrane.annot.MCOtherAttributes;
-                                    
-                                    import java.util.Map;
-                                    
-                                    @MCElement(name = "components", topLevel = true)
-                                    public class Components {
-                                    
-                                        Map<String, Object> components;
-                                    
-                                        public Map<String, Object> getComponents() {
-                                            return components;
-                                        }
-                                    
-                                        @MCOtherAttributes
-                                        public void setComponents(Map<String, Object> components) {
-                                              if (this.components == null)
-                                                this.components = new java.util.LinkedHashMap<>();
-                                              if (components != null)
-                                                this.components.putAll(components);
-                                        }
-                                    }
-                                    """);
-                }
-                return true;
-            } catch (FilerException e) {
-                if (e.getMessage().contains("Source file already created"))
-                    return false;
-                if (e.getMessage().contains("Attempt to recreate a file for"))
-                    return false;
-                throw e;
-            }
-        }
+    @Override
+    protected String getClassName() {
+        return "Components";
+    }
 
-        return false;
+    @Override
+    protected String getClassImpl() {
+        return """
+                import com.predic8.membrane.annot.MCElement;
+                import com.predic8.membrane.annot.MCOtherAttributes;
+                
+                import java.util.Map;
+                
+                @MCElement(name = "components", topLevel = true)
+                public class Components {
+                
+                    Map<String, Object> components;
+                
+                    public Map<String, Object> getComponents() {
+                        return components;
+                    }
+                
+                    @MCOtherAttributes
+                    public void setComponents(Map<String, Object> components) {
+                          if (this.components == null)
+                            this.components = new java.util.LinkedHashMap<>();
+                          if (components != null)
+                            this.components.putAll(components);
+                    }
+                }
+                """;
     }
 }
