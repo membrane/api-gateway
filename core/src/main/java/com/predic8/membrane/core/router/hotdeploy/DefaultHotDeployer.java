f66a966cbc50a961cca09bfd37078ee5560f6c0b
Thomas Bayer <bayer@predic8.de>
2025-12-20 15:38:06 +0100

    Router factor out hotdeploy (#2466)
    * refactor: delegate hot deployment logic to new `HotDeployer` interface and streamline related code in `Router`
    
    * refactor: make `router` field in `DefaultHotDeployer` final for immutability
    
    * fix: ensure proper cleanup and initialization of `hotDeployer` in `Router`
    
    * refactor: enhance `Router` structure with streamlined initialization and reconfiguration logic
    
    * refactor: improve thread synchronization in `DefaultHotDeployer` and add `getLock()` in `Router` for enhanced concurrency handling
    
    * refactor: simplify `DefaultHotDeployer` by removing redundant `hotDeployingContexts` tracking and improve `Router` concurrency management
    
    * refactor: replace `System.exit` calls with `StartupException` in `Router` and `RouterCLI` for cleaner error handling during startup
    
    * refactor: remove redundant `errorNotice` logic and replace `System.exit` with `StartupException` in `RouterCLI` for cleaner error handling
    
    * refactor: extract `tryToGetConfigurationFile` method in `RouterCLI` for cleaner and reusable configuration resolution logic
    
    * refactor: improve error logging in `RouterCLI` and fix indentation for readability
    
    * refactor: replace `StartupException` with `ExitException` for streamlined error handling and debugging in `Router` and `RouterCLI`
    
    * refactor: relocate `ExitException` and rename method in `RouterCLI` for clarity and cohesion
    
    * refactor: improve synchronization and streamline `DefaultHotDeployer` by replacing direct bean factory access with `Router.getRef`
    
    * refactor: add synchronization to `DefaultHotDeployer.start` to prevent concurrent hot deployment initialization
    
    * protect 'running' field by 'lock'
    
    * added error, if Spring Context declares more than 1 router
    
    * added comment
    
    * refactor
    
    * refactor: synchronize `DefaultHotDeployer.stop` and initialize `Router.hotDeployer` with `NullHotDeployer` to enhance thread safety and prevent NPEs
    
    * refactor: simplify `Router.hotDeployer` initialization logic and clarify Javadoc for `setHotDeploy`
    
    * refactor: streamline `Router.hotDeployer` initialization and enhance `HotDeployer` interface with `init` method
    
    ---------
    
    Co-authored-by: Tobias Polley <mail@tobias-polley.de>

---
+++ core/src/main/java/com/predic8/membrane/core/router/hotdeploy/DefaultHotDeployer.java
@@ -0,0 +1,67 @@
+package com.predic8.membrane.core.router.hotdeploy;
+
+import com.predic8.membrane.core.*;
+import com.predic8.membrane.core.config.spring.*;
+import org.slf4j.*;
+
+import javax.annotation.concurrent.*;
+
+public class DefaultHotDeployer implements HotDeployer {
+
+    private static final Logger log = LoggerFactory.getLogger(DefaultHotDeployer.class.getName());
+
+    @GuardedBy("this")
+    private HotDeploymentThread hdt;
+
+    private Router router;
+
+    @Override
+    public void init(Router router) {
+        this.router = router;
+    }
+
+    @Override
+    public void start() {
+        // Prevent multiple threads from starting hot deployment at the same time.
+        synchronized (this) {
+            if (hdt != null)
+                throw new IllegalStateException("Hot deployment already started.");
+
+            if (!(router.getBeanFactory() instanceof TrackingApplicationContext tac)) {
+                log.warn("""
+                        ApplicationContext is not a TrackingApplicationContext. Please set <router hotDeploy="false">.
+                        """);
+                return;
+            }
+
+            hdt = new HotDeploymentThread(router.getRef());
+            hdt.setFiles(tac.getFiles());
+            hdt.start();
+        }
+    }
+
+    @Override
+    public void stop() {
+        synchronized (this) {
+            if (hdt == null)
+                return;
+
+            router.stopAutoReinitializer();
+            hdt.stopASAP();
+            hdt = null;
+        }
+    }
+
+    @Override
+    public void setEnabled(boolean enabled) {
+        if (enabled)
+            start();
+        else
+            stop();
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
