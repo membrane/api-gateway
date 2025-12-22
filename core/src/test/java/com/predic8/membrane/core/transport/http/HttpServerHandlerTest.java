155f37de4bd288225bb8b24bc0509f89d2a9ee2f
Christian Gördes <christian.goerdes@outlook.de>
2025-12-22 17:12:19 +0100

    add test without content-length

--- core/src/test/java/com/predic8/membrane/core/transport/http/HttpServerHandlerTest.java
+++ core/src/test/java/com/predic8/membrane/core/transport/http/HttpServerHandlerTest.java
@@ -1,17 +1,29 @@
 package com.predic8.membrane.core.transport.http;
 
 import com.predic8.membrane.core.Router;
+import com.predic8.membrane.core.exchange.Exchange;
+import com.predic8.membrane.core.http.Request;
 import com.predic8.membrane.core.interceptor.flow.ReturnInterceptor;
+import com.predic8.membrane.core.interceptor.log.LogInterceptor;
 import com.predic8.membrane.core.openapi.serviceproxy.APIProxy;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.when;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class HttpServerHandlerTest {
 
     static Router router;
+
+    HttpClient client;
+
+    @BeforeEach
+    void setUp() {
+        client = new HttpClient();
+    }
 
     @BeforeAll
     static void start() throws Exception {
@@ -20,6 +32,7 @@ class HttpServerHandlerTest {
 
         router.getRuleManager().addProxyAndOpenPortIfNew(new APIProxy() {{
             setPort(2000);
+            getFlow().add(new LogInterceptor());
             getFlow().add(new ReturnInterceptor());
         }});
 
@@ -38,6 +51,14 @@ class HttpServerHandlerTest {
             .post("http://localhost:2000")
         .then()
             .statusCode(200);
+    }
+
+    @Test
+    void init() throws Exception {
+        Exchange exc = Request.post("http://localhost:200").buildExchange();
+        client.initializeRequest(exc,"http://localhost:2000");
+        client.call(exc);
+        assertEquals(200, exc.getResponse().getStatusCode());
     }
 
 }
