e7bd355131a81bc5f05f6b189121194379c329b8
Christian Gördes <christian.goerdes@outlook.de>
2025-12-22 15:51:52 +0100

    prepare empty body test

--- core/src/test/java/com/predic8/membrane/core/http/RequestTest.java
+++ core/src/test/java/com/predic8/membrane/core/http/RequestTest.java
@@ -15,6 +15,7 @@
 package com.predic8.membrane.core.http;
 
 import com.predic8.membrane.core.util.*;
+import org.jetbrains.annotations.NotNull;
 import org.junit.jupiter.api.*;
 
 import java.io.*;
@@ -36,16 +37,65 @@ public class RequestTest {
 
 	private InputStream inPost;
 
+	private InputStream inEmptyPost;
+
 	private InputStream inChunked;
 
 	private ByteArrayOutputStream tempOut;
 
 	private InputStream tempIn;
 
+	private static final String POST_REQUEST = """
+		POST /operation/call HTTP/1.1
+		Host: service-repository.com:80
+		Connection: keep-alive
+		User-Agent: Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US) AppleWebKit/530.5 (KHTML, like Gecko) Chrome/2.0.172.37 Safari/530.5
+		Referer: http://sr/operation/show?operation=getBank&portType=BLZServicePortType&id=65657
+		Content-Length: 168
+		Cache-Control: max-age=0
+		Origin: http://sr
+		Content-Type: application/x-www-form-urlencoded
+		Accept: application/xml,application/xhtml+xml,text/html;q=0.9,text/plain;q=0.8,image/png,*/*;q=0.5
+		Accept-Encoding: gzip,deflate,sdch
+		Cookie: JSESSIONID=E431BCAA27640D0629149A89451239F7
+		Accept-Language: de-DE,de;q=0.8,en-US;q=0.6,en;q=0.4
+		Accept-Charset: ISO-8859-1,utf-8;q=0.7,*;q=0.3
+		
+		endpoint=http%3A%2F%2Fwww.thomas-bayer.com%3A80%2Faxis2%2Fservices%2FBLZService&xpath%3A%2FgetBank%2Fblz=38070024&id=65657&operation=getBank&portType=BLZServicePortType
+		""";
+
+	private static final String CHUNKED_REQUEST = """
+		POST /axis2/services/BLZService HTTP/1.1
+		Content-Type: application/soap+xml; charset=UTF-8; action="http://thomas-bayer.com/blz/BLZServicePortType/getBankRequest"
+		User-Agent: Axis2
+		Host: localhost:7000
+		Transfer-Encoding: chunked
+		
+		ff
+		<?xml version='1.0' encoding='UTF-8'?><soapenv:Envelope xmlns:soapenv="http://www.w3.org/2003/05/soap-envelope"><soapenv:Body><ns1:getBank xmlns:ns1="http://thomas-bayer.com/blz/"><ns1:blz>66762332</ns1:blz></ns1:getBank></soapenv:Body></soapenv:Envelope>
+		0
+		""";
+
+	private static final String POST_EMPTY_BODY_REQUEST = """
+		POST /operation/call HTTP/1.1
+		Host: service-repository.com:80
+		Connection: close
+		User-Agent: test-client/1.0
+		Content-Length: 0
+		Content-Type: application/x-www-form-urlencoded
+		Accept: */*
+		
+		""";
+
 	@BeforeEach
 	public void setUp() {
-		inPost = getResourceAsStream(this,"request-post.msg");
-		inChunked = getResourceAsStream(this,"request-chunked-soap.msg");
+		inPost = getRequest(POST_REQUEST);
+		inEmptyPost = new ByteArrayInputStream(POST_EMPTY_BODY_REQUEST.stripIndent().replace("\n", "\r\n").getBytes());
+		inChunked = getRequest(CHUNKED_REQUEST);
+	}
+
+	private static @NotNull ByteArrayInputStream getRequest(String request) {
+		return new ByteArrayInputStream(request.stripIndent().stripTrailing().replace("\n", "\r\n").getBytes());
 	}
 
 	@AfterEach
@@ -91,6 +141,16 @@ public class RequestTest {
 		assertNotNull(reqPost.getBody());
 
 		assertEquals(168, reqPost.getBody().getLength());
+	}
+
+	@Test
+	void readEmptyPost() throws Exception {
+		reqPost.read(inEmptyPost, true);
+		assertEquals(METHOD_POST, reqPost.getMethod());
+		assertEquals("/operation/call", reqPost.getUri());
+		assertNotNull(reqPost.getBody());
+
+		assertEquals(0, reqPost.getBody().getLength());
 	}
 
 	@Test
@@ -239,7 +299,7 @@ public class RequestTest {
 		AbstractBody originalBody = readMessageAndGetBody();
 		reqPost.setBodyContent("ABC".getBytes(UTF_8));
 		assertTrue(originalBody.isRead()); // Assert that the original body is read
-		assertEquals(0,inPost.available()); // Check that all bytes are read from the stream
+		assertEquals(0, inPost.available()); // Check that all bytes are read from the stream
 	}
 
 	@Test
