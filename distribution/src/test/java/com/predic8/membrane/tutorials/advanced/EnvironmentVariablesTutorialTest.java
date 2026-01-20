6292496b9c1a31f66efa10311e25bae8da910505
Christian Gördes <christian.goerdes@outlook.de>
2026-01-20 16:34:40 +0100

    add tutorialTest

--- distribution/src/test/java/com/predic8/membrane/tutorials/advanced/EnvironmentVariablesTutorialTest.java
+++ distribution/src/test/java/com/predic8/membrane/tutorials/advanced/EnvironmentVariablesTutorialTest.java
@@ -1,5 +1,14 @@
 package com.predic8.membrane.tutorials.advanced;
 
+import com.predic8.membrane.examples.util.ConsoleWatcher;
+import com.predic8.membrane.examples.util.Process2;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.containsString;
+
 public class EnvironmentVariablesTutorialTest extends AbstractAdvancedTutorialTest{
 
     @Override
@@ -7,5 +16,26 @@ public class EnvironmentVariablesTutorialTest extends AbstractAdvancedTutorialTe
         return "90-Environment-Variables.yaml";
     }
 
+    @Test
+    void environmentVariablesAreResolved() {
+        // @formatter:off
+        given()
+        .when()
+            .get("http://localhost:2000/")
+        .then()
+            .statusCode(200)
+            .body(containsString("Hello from port 3000"));
+        // @formatter:on
+    }
+
+    @Override
+    protected Process2 startServiceProxyScript(ConsoleWatcher watch, String script) throws IOException, InterruptedException {
+        Process2.Builder builder = new Process2.Builder().in(baseDir).env("PORT", "3000");
+
+        if (watch != null)
+            builder = builder.withWatcher(watch);
+
+        return builder.script(script).waitForMembrane().parameters("-c %s".formatted(getTutorialYaml())).start();
+    }
 
 }
