2a15716d8e4b64d5613f8dccfe815103b9ca4eb3
Thomas Bayer <bayer@predic8.de>
2026-01-21 11:10:48 +0100

    fix(APIProxy): add null check for `router.getRegistry()` during `xmlConfig` initialization

--- core/src/main/java/com/predic8/membrane/core/openapi/serviceproxy/APIProxy.java
+++ core/src/main/java/com/predic8/membrane/core/openapi/serviceproxy/APIProxy.java
@@ -86,7 +86,7 @@ public class APIProxy extends ServiceProxy implements Polyglot, XMLSupport {
     @Override
     public void init() {
         super.init();
-        if (xmlConfig == null) {
+        if (xmlConfig == null && router.getRegistry() != null) {
             xmlConfig = router.getRegistry().getBean(XmlConfig.class).orElse(null);
         }
         if (test != null && !test.isEmpty()) {
