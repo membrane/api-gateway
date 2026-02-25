2b40c0005e9ce9800769430d0ce52a90661a5b88
Christian Gördes <christian.goerdes@outlook.de>
2026-02-25 16:01:04 +0100

    Add schema version support in JSON schema generation

--- annot/src/main/java/com/predic8/membrane/annot/generator/kubernetes/model/Schema.java
+++ annot/src/main/java/com/predic8/membrane/annot/generator/kubernetes/model/Schema.java
@@ -1,16 +1,3 @@
-/* Copyright 2009, 2021 predic8 GmbH, www.predic8.com
-
-   Licensed under the Apache License, Version 2.0 (the "License");
-   you may not use this file except in compliance with the License.
-   You may obtain a copy of the License at
-
-   http://www.apache.org/licenses/LICENSE-2.0
-
-   Unless required by applicable law or agreed to in writing, software
-   distributed under the License is distributed on an "AS IS" BASIS,
-   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-   See the License for the specific language governing permissions and
-   limitations under the License. */
 package com.predic8.membrane.annot.generator.kubernetes.model;
 
 import com.fasterxml.jackson.databind.node.ObjectNode;
@@ -23,6 +10,7 @@ import static com.predic8.membrane.annot.generator.kubernetes.model.SchemaFactor
 public class Schema extends SchemaObject {
 
     private final List<ISchema> definitions = new ArrayList<>();
+    private String version;
 
     Schema(String name) {
         super(name);
@@ -34,11 +22,19 @@ public class Schema extends SchemaObject {
         return this;
     }
 
+    public Schema version(String version) {
+        this.version = version;
+        return this;
+    }
+
     @Override
     public ObjectNode json(ObjectNode node) {
         node.put("$id", "https://membrane-soa.org/%s.schema.json".formatted(name));
         node.put("$schema", "https://json-schema.org/draft/2020-12/schema");
         node.put("title", name);
+        if (version != null && !version.isBlank()) {
+            node.put("version", version);
+        }
         super.json(node);
         if (!definitions.isEmpty()) {
             ObjectNode defs = jnf.objectNode();
