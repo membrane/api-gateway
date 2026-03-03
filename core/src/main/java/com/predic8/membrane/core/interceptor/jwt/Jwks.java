ac0c7b7223816e4111bc2f3e06324aacb874f5ec
Valentin Brückel <brueckel@predic8.de>
2026-03-03 09:55:41 +0100

    add null check

--- core/src/main/java/com/predic8/membrane/core/interceptor/jwt/Jwks.java
+++ core/src/main/java/com/predic8/membrane/core/interceptor/jwt/Jwks.java
@@ -97,6 +97,8 @@ public class Jwks {
     @MCChildElement
     public Jwks setJwks(List<Jwk> jwks) {
         this.jwks = jwks;  // unnecessary, mainly for consistency when debugging
+        if (jwks == null)
+            throw new ConfigurationException("JWKs list must not be null.");
         if (router != null)  // set in init, so we can't update prior to that call
             this.keysByKid = buildKeyMap(jwks);
         return this;
