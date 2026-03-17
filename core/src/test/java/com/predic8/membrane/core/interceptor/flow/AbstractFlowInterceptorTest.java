b2a3680b7da10e654649bff6629d2ec8997f3aa6
Thomas Bayer <bayer@predic8.de>
2026-03-17 14:23:41 +0100

    Update `AbstractFlowInterceptorTest` to validate propagation of proxy and router
    - Renamed test method for clarity and updated logic to use `setFlow` with `List.of`.
    - Verified proper initialization of nested interceptors with proxy and router.

--- core/src/test/java/com/predic8/membrane/core/interceptor/flow/AbstractFlowInterceptorTest.java
+++ core/src/test/java/com/predic8/membrane/core/interceptor/flow/AbstractFlowInterceptorTest.java
@@ -1,23 +1,23 @@
 package com.predic8.membrane.core.interceptor.flow;
 
-import com.predic8.membrane.core.interceptor.*;
 import com.predic8.membrane.core.interceptor.log.*;
 import com.predic8.membrane.core.proxies.*;
 import com.predic8.membrane.core.router.*;
-import groovy.util.logging.*;
 import org.junit.jupiter.api.*;
+
+import java.util.*;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class AbstractFlowInterceptorTest {
 
     @Test
-    void propagationOfProxy() {
+    void propagationOfProxyAndRouterToNestedInterceptors() {
         var r = new TestRouter();
         var p = new ServiceProxy();
         var ri = new RequestInterceptor();
         var ti = new LogInterceptor();
-        ri.interceptors.add(ti);
+        ri.setFlow(List.of(ti));
         ri.init(r,p);
         assertSame(r,ti.getRouter());
         assertSame(p,ti.getProxy());
