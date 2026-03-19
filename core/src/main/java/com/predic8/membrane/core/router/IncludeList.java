5bdd15ff3bb45e55237ce2e862953945960059a0
Christian Gördes <christian.goerdes@outlook.de>
2026-03-19 11:51:46 +0100

    add documenation

--- core/src/main/java/com/predic8/membrane/core/router/IncludeList.java
+++ core/src/main/java/com/predic8/membrane/core/router/IncludeList.java
@@ -7,7 +7,25 @@ import java.util.ArrayList;
 import java.util.List;
 
 /**
- * @description TODO
+ * @description <p>
+ * Includes additional YAML configuration files before parsing the current file's own configuration.
+ * </p>
+ * <p>
+ * Include entries are resolved in the order they are listed. If an entry points to a directory,
+ * all matching <code>*.apis.yaml</code> / <code>*.apis.yml</code> files in that directory are included.
+ * For directory includes, no ordering is applied. Includes are resolved recursively and cyclic
+ * include chains are rejected.
+ * </p>
+ * <p>
+ * Paths used inside included configuration content (for example OpenAPI file locations or other
+ * referenced resources) are resolved against the base path of the main configuration file.
+ * </p>
+ * @yaml <pre><code>
+ * include:
+ *   - "."
+ *   - apis/demo.apis.yaml
+ *   - other/apis
+ * </code></pre>
  */
 @MCElement(name = "include", topLevel = true, noEnvelope = true, component = false)
 public class IncludeList {
@@ -15,7 +33,13 @@ public class IncludeList {
     List<String> includes = new ArrayList<>();
 
     /**
-     * @description TODO
+     * @description <p>
+     * Declares include entries as a list of strings.
+     * </p>
+     * <p>
+     * Each string is a path to either a YAML file or a directory. Relative paths are resolved
+     * against the directory of the including file. Absolute paths are also supported.
+     * </p>
      */
     @MCChildElement(allowForeign = true)
     public void setInclude(List<String> include) {
