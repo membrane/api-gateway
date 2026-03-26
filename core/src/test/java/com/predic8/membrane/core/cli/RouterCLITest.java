8f19cf1cac24a7ba9d8db0325ff01166a754e506
thomas <bayer@predic8.de>
2026-03-26 18:31:38 +0100

    fix: fixex #2892

---
+++ core/src/test/java/com/predic8/membrane/core/cli/RouterCLITest.java
@@ -0,0 +1,56 @@
+/* Copyright 2023 predic8 GmbH, www.predic8.com
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+   http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License. */
+
+package com.predic8.membrane.core.cli;
+
+import com.predic8.membrane.test.TestAppender;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.core.Logger;
+import org.junit.jupiter.api.Test;
+
+import java.time.Duration;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class RouterCLITest {
+
+    @Test
+    void getUserDir() {
+        assertTrue(RouterCLI.getUserDir().endsWith("/"));
+    }
+
+    /**
+     * Tests if the basepath is set on the configuration object. If not the resolving of
+     * the openapi file in the config will fail.
+     */
+    @Test
+    void basepath() throws Exception {
+        var logger = (Logger) LogManager.getRootLogger();
+
+        var appender = new TestAppender("TestAppender");
+        appender.start();
+        logger.addAppender(appender);
+
+        var t = new Thread(() -> {
+            try {
+                RouterCLI.main(new String[]{"-c", "src/test/resources/configuration/config.apis.yaml"});
+            } catch (Exception e) {
+                throw new RuntimeException(e);
+            }
+        });
+        t.start();
+
+       appender.awaitContainsOrThrow("running", Duration.ofSeconds(10));
+    }
+}
