75afdcf24256c359c4ad50a9cb4f0837cec9e857
thomas <bayer@predic8.de>
2026-09-24 20:06:39 +0200

    Refactor `LimitedMemoryExchangeStoreIntegrationTest` for improved readability and flow

--- core/src/test/java/com/predic8/membrane/integration/withoutinternet/LimitedMemoryExchangeStoreIntegrationTest.java
+++ core/src/test/java/com/predic8/membrane/integration/withoutinternet/LimitedMemoryExchangeStoreIntegrationTest.java
@@ -14,12 +14,13 @@
 
 package com.predic8.membrane.integration.withoutinternet;
 
-import com.predic8.membrane.core.exchange.AbstractExchange;
 import com.predic8.membrane.core.exchange.Exchange;
 import com.predic8.membrane.core.exchangestore.LimitedMemoryExchangeStore;
-import com.predic8.membrane.core.interceptor.*;
+import com.predic8.membrane.core.interceptor.AbstractInterceptor;
+import com.predic8.membrane.core.interceptor.HTTPClientInterceptor;
+import com.predic8.membrane.core.interceptor.Outcome;
 import com.predic8.membrane.core.interceptor.flow.RequestInterceptor;
-import com.predic8.membrane.core.interceptor.flow.ResponseInterceptor;
+import com.predic8.membrane.core.interceptor.flow.ReturnInterceptor;
 import com.predic8.membrane.core.interceptor.templating.StaticInterceptor;
 import com.predic8.membrane.core.proxies.ServiceProxy;
 import com.predic8.membrane.core.proxies.ServiceProxyKey;
@@ -44,35 +45,34 @@ import static java.lang.Integer.MAX_VALUE;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-public class LimitedMemoryExchangeStoreIntegrationTest {
-    private static LimitedMemoryExchangeStore lmes;
-    private static DefaultRouter router;
-    private static DefaultRouter router2;
-    private static HttpClientConfiguration hcc;
-    private static final AtomicReference<Exchange> middleExchange = new AtomicReference<>();
+class LimitedMemoryExchangeStoreIntegrationTest {
+    private LimitedMemoryExchangeStore lmes;
+    private DefaultRouter router;
+    private DefaultRouter router2;
+    private HttpClientConfiguration hcc;
+    private final AtomicReference<Exchange> middleExchange = new AtomicReference<>();
 
     @BeforeEach
     void setup() throws Exception {
         lmes = new LimitedMemoryExchangeStore();
         lmes.setMaxSize(500_000);
-        lmes.removeAllExchanges((AbstractExchange[]) lmes.getAllExchanges());
         // streaming only works when no retry can happen, i.e. retries = 0
         hcc = new HttpClientConfiguration();
         hcc.getRetryHandler().setRetries(0);
 
-        ServiceProxy proxy = getServiceProxy(3045, "localhost", 80);
+        var proxy = getServiceProxy(3045, "localhost", 80);
         var ri = new RequestInterceptor();
         var si = new StaticInterceptor();
         si.setSrc("Dummy");
         ri.getFlow().add(si);
-        ri.getFlow().add(new ResponseInterceptor());
+        ri.getFlow().add(new ReturnInterceptor());
         proxy.getFlow().add(ri);
         router = new DefaultRouter();
         router.add(proxy);
         router.start();
         setClientConfig(router, hcc);
 
-        ServiceProxy proxy1 = getServiceProxy(3046, "localhost", 3045);
+        var proxy1 = getServiceProxy(3046, "localhost", 3045);
         proxy1.getFlow().add(new AbstractInterceptor() {
             @Override
             public Outcome handleRequest(Exchange exc) {
@@ -85,9 +85,6 @@ public class LimitedMemoryExchangeStoreIntegrationTest {
         router2.add(proxy1);
         router2.start();
         setClientConfig(router2, hcc);
-        var global = new GlobalInterceptor();
-        global.getFlow().add(new ExchangeStoreInterceptor(lmes));
-        router.getRegistry().register("global", global);
     }
 
     private static void setClientConfig(Router router, HttpClientConfiguration hcc) {
@@ -95,8 +92,8 @@ public class LimitedMemoryExchangeStoreIntegrationTest {
         client.updateHttpClientConfig(hcc);
     }
 
-    private static @NotNull ServiceProxy getServiceProxy(int port, String localhost, int targetPort) {
-        return new ServiceProxy(new ServiceProxyKey("localhost", "POST", ".*", port), localhost, targetPort);
+    private static @NotNull ServiceProxy getServiceProxy(int port, String targetHost, int targetPort) {
+        return new ServiceProxy(new ServiceProxyKey("localhost", "POST", ".*", port), targetHost, targetPort);
     }
 
     private static @NotNull HTTPClientInterceptor getHttpClientInterceptor(Router router) {
@@ -138,10 +135,11 @@ public class LimitedMemoryExchangeStoreIntegrationTest {
         return post("http://localhost:3046/foo").body(len, new LargeBodyTest.ConstantInputStream(len));
     }
 
-    private static void call(Exchange e) throws Exception {
+    private void call(Exchange e) throws Exception {
         try (HttpClient hc = new HttpClient(hcc)) {
             hc.call(e);
         }
+        assertEquals(200, e.getResponse().getStatusCode());
         assertTrue(e.getRequest().getBody().wasStreamed());
         assertTrue(middleExchange.get().getRequest().getBody().wasStreamed());
     }
